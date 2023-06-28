1ce3843cab25813ef5451ad270ad771f4227a6df
laurent <laurent.mazare@gmail.com>
2023-06-28 09:38:54 +0100

    Add the relu op.

--- candle-core/src/tensor.rs
+++ candle-core/src/tensor.rs
@@ -320,6 +320,7 @@ impl Tensor {
     unary_op!(sqr, Sqr);
     unary_op!(sqrt, Sqrt);
     unary_op!(gelu, Gelu);
+    unary_op!(relu, Relu);
     pub fn to_scalar<S: crate::WithDType>(&self) -> Result<S> {
         if self.rank() != 0 {
             return Err(Error::UnexpectedNumberOfDims {
