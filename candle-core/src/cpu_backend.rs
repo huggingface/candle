1c755c0e5bb060e1676ef6583e0d2007a5016026
laurent <laurent.mazare@gmail.com>
2023-06-28 14:33:06 +0100

    Remove some todos.

--- candle-core/src/cpu_backend.rs
+++ candle-core/src/cpu_backend.rs
@@ -577,7 +577,6 @@ impl CpuStorage {
         layout_f: &Layout,
     ) -> Result<Self> {
         // TODO: Support types that could be casted to a boolean.
-        // TODO: this should use the layout.
         let pred = self.as_slice::<u32>()?;
         match (t, f) {
             (Self::BF16(t), Self::BF16(f)) => {
@@ -615,7 +614,6 @@ impl CpuStorage {
         hidden_size: usize,
         vocab_size: usize,
     ) -> Result<Self> {
-        // TODO: this should use the layout.
         let ids = self.as_slice::<u32>()?;
         map1!(vs, take_impl1, ids, layout, vocab_size, hidden_size)
     }
