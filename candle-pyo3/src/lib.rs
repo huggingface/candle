ebb0fedf145f0e6d2b332e884bf62009dd33d477
laurent <laurent.mazare@gmail.com>
2023-07-01 20:36:44 +0100

    Very simple pyo3 bindings for candle.

---
+++ candle-pyo3/src/lib.rs
@@ -0,0 +1,46 @@
+use pyo3::prelude::*;
+use pyo3::{
+    exceptions::PyValueError,
+};
+
+use ::candle::{Tensor, Device::Cpu};
+
+pub fn wrap_err(err: ::candle::Error) -> PyErr {
+    PyErr::new::<PyValueError, _>(format!("{err:?}"))
+}
+
+#[pyclass(name="Tensor")]
+struct PyTensor(Tensor);
+
+impl std::ops::Deref for PyTensor {
+    type Target = Tensor;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[pymethods]
+impl PyTensor {
+    #[new]
+    fn new(f: f32) -> PyResult<Self> {
+        Ok(Self(Tensor::new(f, &Cpu).map_err(wrap_err)?))
+    }
+
+    fn __repr__(&self) -> String {
+        format!("{}", self.0)
+    }
+}
+
+#[pyfunction]
+fn add(tensor: &PyTensor, f: f64) -> PyResult<PyTensor> {
+    let tensor = (&tensor.0 + f).map_err(wrap_err)?;
+    Ok(PyTensor(tensor))
+}
+
+#[pymodule]
+fn candle(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyTensor>()?;
+    m.add_function(wrap_pyfunction!(add, m)?)?;
+    Ok(())
+}
---
+++ candle-pyo3/test.py
@@ -0,0 +1,5 @@
+import candle
+
+t = candle.Tensor(42.0)
+print(t)
+print(candle.add(t, 3.14))
