d71b31144d286df98d2ae12bde66976794f33640
laurent <laurent.mazare@gmail.com>
2023-07-04 09:29:19 +0100

    Add a weight extraction script.

--- candle-examples/examples/whisper/main.rs
+++ candle-examples/examples/whisper/main.rs
@@ -96,6 +96,23 @@ struct Config {
     n_text_layer: usize,
 }
 
+impl Config {
+    fn tiny() -> Self {
+        Self {
+            n_mels: 80,
+            n_vocab: 51864,
+            n_audio_ctx: 1500,
+            n_audio_state: 384,
+            n_audio_head: 6,
+            n_audio_layer: 4,
+            n_text_ctx: 448,
+            n_text_state: 384,
+            n_text_head: 6,
+            n_text_layer: 4,
+        }
+    }
+}
+
 struct Embedding {
     embeddings: Tensor,
     hidden_size: usize,
