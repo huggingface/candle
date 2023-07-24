746a9d9bf42dc69f404699d7244f19c99366b8d6
laurent <laurent.mazare@gmail.com>
2023-07-24 11:08:23 +0100

    Move the whisper example.

--- candle-wasm-example/src/bin/worker.rs
+++
@@ -1,4 +0,0 @@
-use yew_agent::PublicWorker;
-fn main() {
-    candle_wasm_example::Worker::register();
-}
--- candle-wasm-examples/whisper/src/bin/app.rs
+++ candle-wasm-examples/whisper/src/bin/app.rs
@@ -1,4 +1,4 @@
 fn main() {
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
-    yew::Renderer::<candle_wasm_example::App>::new().render();
+    yew::Renderer::<candle_wasm_example_whisper::App>::new().render();
 }
