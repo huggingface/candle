85014fad8c323131f751435f05303c968d476760
laurent <laurent.mazare@gmail.com>
2023-07-28 11:13:08 +0100

    Slightly simpler softmax.

--- candle-examples/examples/bigcode/model.rs
+++ candle-examples/examples/bigcode/model.rs
@@ -35,9 +35,9 @@ fn softmax<D: candle::shape::Dim>(xs: &Tensor, d: D) -> Result<Tensor> {
     let d = d.to_index(xs.shape(), "log-softmax")?;
     let max = xs.max_keepdim(d)?;
     let diff = xs.broadcast_sub(&max)?;
-    let sum_exp = diff.exp()?.sum_keepdim(d)?;
-    let log_sm = diff.broadcast_sub(&sum_exp.log()?)?;
-    log_sm.exp()
+    let num = diff.exp()?;
+    let den = num.sum_keepdim(d)?;
+    num.broadcast_div(&den)
 }
 
 #[derive(Debug)]
