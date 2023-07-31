ffeafbfc43307fe4e2daa3e3fdfe7afb781c5505
Laurent Mazare <laurent.mazare@gmail.com>
2023-07-31 14:14:01 +0100

    Make the nll op closer to the pytorch version + add a test. (#286)

---
+++ candle-nn/tests/loss.rs
@@ -0,0 +1,31 @@
+use candle::{Device, Result, Tensor};
+
+/* Equivalent python code:
+import torch
+import torch.nn.functional as F
+input = torch.tensor([
+    [ 1.1050,  0.3013, -1.5394, -2.1528, -0.8634],
+    [ 1.0730, -0.9419, -0.1670, -0.6582,  0.5061],
+    [ 0.8318,  1.1154, -0.3610,  0.5351,  1.0830]])
+
+target = torch.tensor([1, 0, 4])
+print(F.nll_loss(F.log_softmax(input, dim=1), target))
+*/
+#[test]
+fn nll() -> Result<()> {
+    let cpu = Device::Cpu;
+    let input = Tensor::new(
+        &[
+            [1.1050f32, 0.3013, -1.5394, -2.1528, -0.8634],
+            [1.0730, -0.9419, -0.1670, -0.6582, 0.5061],
+            [0.8318, 1.1154, -0.3610, 0.5351, 1.0830],
+        ],
+        &cpu,
+    )?;
+    let target = Tensor::new(&[1u32, 0, 4], &cpu)?;
+
+    let log_softmax = candle_nn::ops::log_softmax(&input, 1)?;
+    let loss = candle_nn::loss::nll(&log_softmax, &target)?;
+    assert_eq!(loss.to_vec0::<f32>()?, 1.1312335);
+    Ok(())
+}
