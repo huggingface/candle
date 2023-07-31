3b6b8a37fc24f6fb1c2cd0d46e55da64abea5d77
laurent <laurent.mazare@gmail.com>
2023-07-31 08:22:46 +0100

    Expose the seqlen variable for flash-attn without padding.

--- candle-flash-attn/src/ffi.rs
+++ candle-flash-attn/src/ffi.rs
@@ -7,6 +7,8 @@ extern "C" {
         v_ptr: *const c_void,
         o_ptr: *const c_void,
         softmax_lse_ptr: *const c_void,
+        cu_seqlens_q_ptr: *const i32,
+        cu_seqlens_k_ptr: *const i32,
 
         q_batch_stride: u32,
         k_batch_stride: u32,
