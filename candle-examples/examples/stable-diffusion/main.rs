2ae3ed9b1ccab07c191bfd5025cbd976bcc84a28
Laurent <laurent.mazare@gmail.com>
2023-08-06 11:05:42 +0100

    Start adding a stable-diffusion example.

---
+++ candle-examples/examples/stable-diffusion/main.rs
@@ -0,0 +1,23 @@
+#[cfg(feature = "mkl")]
+extern crate intel_mkl_src;
+
+mod clip;
+
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Run on CPU rather than on GPU.
+    #[arg(long)]
+    cpu: bool,
+
+    #[arg(long)]
+    prompt: String,
+}
+
+fn main() -> Result<()> {
+    let _args = Args::parse();
+    Ok(())
+}
