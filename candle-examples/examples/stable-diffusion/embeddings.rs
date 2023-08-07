86026c8057b8663bdd5d95e4697a1c00b11b1281
Laurent <laurent.mazare@gmail.com>
2023-08-07 16:19:04 +0200

    Fix the tensor indexing when padding.

--- candle-examples/examples/stable-diffusion/embeddings.rs
+++ candle-examples/examples/stable-diffusion/embeddings.rs
@@ -57,7 +57,7 @@ impl Timesteps {
             Tensor::cat(&[&sin, &cos], D::Minus1)?
         };
         if self.num_channels % 2 == 1 {
-            emb.pad_with_zeros(1, 0, 1)
+            emb.pad_with_zeros(D::Minus2, 0, 1)
         } else {
             Ok(emb)
         }
