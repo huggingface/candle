e2d9001deec6975d1dceaf39fc4ca29cafc6adcc
laurent <laurent.mazare@gmail.com>
2023-08-14 11:09:10 +0100

    Add a cuda kernel for avg-pool2d.

--- candle-kernels/src/conv.cu
+++ candle-kernels/src/conv.cu
@@ -61,9 +61,6 @@ __device__ void conv2d(
     T *dst
 ) {
   const size_t dst_i = blockIdx.x * blockDim.x + threadIdx.x;
-  if (dst_i >= src_numel) {
-    return;
-  }
   // src: (b_size, c_in, w_in, h_in)
   // k: (c_out, c_in, w_k, h_k)
   const size_t *src_dims = info;
@@ -107,6 +104,57 @@ __device__ void conv2d(
   dst[dst_i] = static_cast<T>(d);
 }
 
+template <typename T, typename A>
+__device__ void avg_pool2d(
+    const size_t src_numel,
+    const size_t w_k,
+    const size_t h_k,
+    const size_t w_stride,
+    const size_t h_stride,
+    const size_t *info,
+    const T *src,
+    T *dst
+) {
+  const size_t dst_i = blockIdx.x * blockDim.x + threadIdx.x;
+  // src: (b_size, c_in, w_in, h_in)
+  const size_t *src_dims = info;
+  const size_t *src_s = info + 4;
+
+  const size_t c = src_dims[1];
+  const size_t w_in = src_dims[2];
+  const size_t h_in = src_dims[3];
+
+  const size_t w_out = (w_in - w_k) / w_stride + 1;
+  const size_t h_out = (h_in - h_k) / h_stride + 1;
+
+  // TODO: Improve this.
+  const size_t b_idx = dst_i / (w_out * h_out * c);
+  const size_t c_idx = (dst_i / (w_out * h_out)) % c;
+  const size_t dst_w = (dst_i / h_out) % w_out;
+  const size_t dst_h = dst_i % h_out;
+
+  const size_t src_idx0 = b_idx * src_s[0];
+  const float scale = 1.0 / (w_k * h_k);
+  A d = 0;
+  for (size_t w_offset = 0; w_offset < w_k; ++w_offset) {
+    size_t src_w = w_stride * dst_w + w_offset;
+    if (src_w < w_k / 2 || src_w >= w_in + w_k / 2) {
+      continue;
+    }
+    src_w -= w_k / 2;
+    for (size_t h_offset = 0; h_offset < h_k; ++h_offset) {
+      size_t src_h = h_stride * dst_h + h_offset;
+      if (src_h < h_k / 2 || src_h >= h_in + h_k / 2) {
+        continue;
+      }
+      src_h -= h_k / 2;
+      const size_t src_idx = src_idx0 + c_idx * src_s[1] + src_w * src_s[2] + src_h * src_s[3];
+      d += static_cast<A>(src[src_idx]);
+    }
+  }
+  dst[dst_i] = static_cast<T>(d * scale);
+}
+
 
 #define CONV1D_OP(TYPENAME, TYPEACC, FN_NAME) \
 extern "C" __global__ void FN_NAME(  \
@@ -137,14 +185,30 @@ extern "C" __global__ void FN_NAME(  \
   conv2d<TYPENAME, TYPEACC>(src_numel, w_out, h_out, stride, padding, info, src, kernel, dst); \
 } \
 
+#define AVG_POOL2D_OP(TYPENAME, TYPEACC, FN_NAME) \
+extern "C" __global__ void FN_NAME(  \
+    const size_t src_numel, \
+    const size_t w_k, \
+    const size_t h_k, \
+    const size_t w_stride, \
+    const size_t h_stride, \
+    const size_t *info, \
+    const TYPENAME *src, \
+    TYPENAME *dst \
+) {  \
+  avg_pool2d<TYPENAME, TYPEACC>(src_numel, w_k, h_k, w_stride, h_stride, info, src, dst); \
+} \
+
 #if __CUDA_ARCH__ >= 800
 CONV1D_OP(__nv_bfloat16, float, conv1d_bf16)
 CONV2D_OP(__nv_bfloat16, float, conv2d_bf16)
+AVG_POOL2D_OP(__nv_bfloat16, float, avg_pool2d_bf16)
 #endif
 
 #if __CUDA_ARCH__ >= 530
 CONV1D_OP(__half, float, conv1d_f16)
 CONV2D_OP(__half, float, conv2d_f16)
+AVG_POOL2D_OP(__half, float, avg_pool2d_f16)
 #endif
 
 CONV1D_OP(float, float, conv1d_f32)
@@ -157,3 +221,7 @@ CONV2D_OP(double, double, conv2d_f64)
 CONV2D_OP(uint8_t, uint8_t, conv2d_u8)
 CONV2D_OP(uint32_t, uint32_t, conv2d_u32)
 
+AVG_POOL2D_OP(float, float, avg_pool2d_f32)
+AVG_POOL2D_OP(double, double, avg_pool2d_f64)
+AVG_POOL2D_OP(uint8_t, uint8_t, avg_pool2d_u8)
+AVG_POOL2D_OP(uint32_t, uint32_t, avg_pool2d_u32)
