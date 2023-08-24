dacfd2dd5b4c05081e71d36e5ad5ced2b4f071bd
laurent <laurent.mazare@gmail.com>
2023-08-24 11:12:57 +0100

    Tweak.

--- candle-core/examples/cuda_basics.rs
+++ candle-core/examples/cuda_basics.rs
@@ -10,7 +10,7 @@ use candle_core::{Device, Tensor};
 fn main() -> Result<()> {
     let device = Device::new_cuda(0)?;
     let in_t = Tensor::rand(-1f32, 1f32, (1, 3, 12, 7), &device)?;
-    let k_t = Tensor::rand(-1f32, 1f32, (6, 3, 1, 7), &device)?;
+    let k_t = Tensor::rand(-1f32, 1f32, (6, 3, 1, 1), &device)?;
     let out_t = in_t.conv2d(&k_t, 0, 1, 1)?;
     println!("{out_t}");
     let in_t = in_t.to_device(&Device::Cpu)?;
