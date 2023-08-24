6316102317c4a2abb051aded16f48c358f9da836
laurent <laurent.mazare@gmail.com>
2023-08-24 12:03:58 +0100

    Display the error.

--- candle-core/examples/cuda_basics.rs
+++ candle-core/examples/cuda_basics.rs
@@ -15,8 +15,11 @@ fn main() -> Result<()> {
     println!("{out_t}");
     let in_t = in_t.to_device(&Device::Cpu)?;
     let k_t = k_t.to_device(&Device::Cpu)?;
-    let out_t = in_t.conv2d(&k_t, 0, 1, 1)?;
-    println!("{out_t}");
+    let out_t2 = in_t.conv2d(&k_t, 0, 1, 1)?;
+    let diff = (out_t.to_device(&Device::Cpu)? - out_t2)?
+        .sqr()?
+        .sum_all()?;
+    println!("{diff}");
 
     let t = Tensor::randn(0f32, 1f32, (2, 4, 96, 96), &device)?;
     let w = Tensor::randn(0f32, 1f32, (320, 4, 3, 3), &device)?;
