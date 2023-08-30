c33201e9670e53508942d866ea9034727bbd9a8d
laurent <laurent.mazare@gmail.com>
2023-08-30 13:17:29 +0100

    Implement the RNN forward pass.

--- candle-nn/src/rnn.rs
+++ candle-nn/src/rnn.rs
@@ -1,5 +1,5 @@
 //! Recurrent Neural Networks
-use candle::{DType, Device, Result, Tensor};
+use candle::{DType, Device, IndexOp, Result, Tensor};
 
 /// Trait for Recurrent Neural Networks.
 #[allow(clippy::upper_case_acronyms)]
@@ -32,7 +32,7 @@ pub trait RNN {
 
 /// The state for a LSTM network, this contains two tensors.
 #[allow(clippy::upper_case_acronyms)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct LSTMState {
     h: Tensor,
     c: Tensor,
@@ -53,7 +53,6 @@ impl LSTMState {
 #[allow(clippy::upper_case_acronyms)]
 #[derive(Debug, Clone, Copy)]
 pub struct LSTMConfig {
-    pub has_biases: bool,
     pub w_ih_init: super::Init,
     pub w_hh_init: super::Init,
     pub b_ih_init: Option<super::Init>,
@@ -63,11 +62,21 @@ pub struct LSTMConfig {
 impl Default for LSTMConfig {
     fn default() -> Self {
         Self {
-            has_biases: true,
             w_ih_init: super::init::DEFAULT_KAIMING_UNIFORM,
             w_hh_init: super::init::DEFAULT_KAIMING_UNIFORM,
             b_ih_init: Some(super::Init::Const(0.)),
             b_hh_init: Some(super::Init::Const(0.)),
+        }
+    }
+}
+
+impl LSTMConfig {
+    pub fn default_no_bias() -> Self {
+        Self {
+            w_ih_init: super::init::DEFAULT_KAIMING_UNIFORM,
+            w_hh_init: super::init::DEFAULT_KAIMING_UNIFORM,
+            b_ih_init: None,
+            b_hh_init: None,
         }
     }
 }
@@ -78,7 +87,10 @@ impl Default for LSTMConfig {
 #[allow(clippy::upper_case_acronyms, unused)]
 #[derive(Debug)]
 pub struct LSTM {
-    flat_weights: Vec<Tensor>,
+    w_ih: Tensor,
+    w_hh: Tensor,
+    b_ih: Option<Tensor>,
+    b_hh: Option<Tensor>,
     hidden_dim: usize,
     config: LSTMConfig,
     device: Device,
@@ -87,14 +99,34 @@ pub struct LSTM {
 
 /// Creates a LSTM layer.
 pub fn lstm(
-    _in_dim: usize,
+    in_dim: usize,
     hidden_dim: usize,
     config: LSTMConfig,
     vb: crate::VarBuilder,
 ) -> Result<LSTM> {
-    let flat_weights = vec![]; // TODO
+    let w_ih = vb.get_with_hints(
+        (4 * hidden_dim, in_dim),
+        "weight_ih_l0", // Only a single layer is supported.
+        config.w_ih_init,
+    )?;
+    let w_hh = vb.get_with_hints(
+        (4 * hidden_dim, in_dim),
+        "weight_hh_l0", // Only a single layer is supported.
+        config.w_hh_init,
+    )?;
+    let b_ih = match config.b_ih_init {
+        Some(init) => Some(vb.get_with_hints(4 * hidden_dim, "bias_ih_l0", init)?),
+        None => None,
+    };
+    let b_hh = match config.b_hh_init {
+        Some(init) => Some(vb.get_with_hints(4 * hidden_dim, "bias_hh_l0", init)?),
+        None => None,
+    };
     Ok(LSTM {
-        flat_weights,
+        w_ih,
+        w_hh,
+        b_ih,
+        b_hh,
         hidden_dim,
         config,
         device: vb.device().clone(),
@@ -119,7 +151,40 @@ impl RNN for LSTM {
         Ok(state)
     }
 
-    fn seq_init(&self, _input: &Tensor, _in_state: &Self::State) -> Result<(Tensor, Self::State)> {
-        todo!()
+    /// The input should have dimensions [batch_size, seq_len, features].
+    fn seq_init(&self, input: &Tensor, in_state: &Self::State) -> Result<(Tensor, Self::State)> {
+        let (_b_size, seq_len, _features) = input.dims3()?;
+        let mut c = in_state.c.clone();
+        let mut h = in_state.h.clone();
+        let mut output: Vec<Tensor> = Vec::with_capacity(seq_len);
+        for seq_index in 0..seq_len {
+            let input = input.i((.., seq_index, ..))?;
+            let w_ih = input.matmul(&self.w_ih.t()?)?;
+            let w_hh = input.matmul(&self.w_hh.t()?)?;
+            let w_ih = match &self.b_ih {
+                None => w_ih,
+                Some(b_ih) => w_ih.broadcast_add(b_ih)?,
+            };
+            let w_hh = match &self.b_hh {
+                None => w_hh,
+                Some(b_hh) => w_hh.broadcast_add(b_hh)?,
+            };
+            let chunks = (&w_ih + &w_hh)?.chunk(4, 1)?;
+            let in_gate = crate::ops::sigmoid(&chunks[0])?;
+            let forget_gate = crate::ops::sigmoid(&chunks[1])?;
+            // TODO: This should be a tanh
+            let cell_gate = crate::ops::sigmoid(&chunks[2])?;
+            let out_gate = crate::ops::sigmoid(&chunks[3])?;
+
+            let next_c = ((forget_gate * c)? + (in_gate * cell_gate)?)?;
+            let next_h = (out_gate * crate::ops::sigmoid(&next_c)?)?;
+
+            c = next_c;
+            h = next_h;
+            output.push(h.clone());
+        }
+        let output = Tensor::cat(&output, 1)?;
+        let out_state = LSTMState { h, c };
+        Ok((output, out_state))
     }
 }
