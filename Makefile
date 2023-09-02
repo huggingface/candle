21109e19834ad852e54daef7c7729b535e2241ba
Laurent Mazare <laurent.mazare@gmail.com>
2023-09-02 17:19:35 +0200

    Recommend using maturin. (#717)

--- Makefile
+++ Makefile
@@ -1,14 +1,4 @@
-pyo3-test:
-	cargo build --profile=release-with-debug --package candle-pyo3
-	cp -f ./target/release-with-debug/libcandle.so candle.so
-	PYTHONPATH=.:$PYTHONPATH python3 candle-pyo3/test.py
-
-pyo3-test-macos:
-	cargo build --profile=release-with-debug --package candle-pyo3
-	cp -f ./target/release-with-debug/libcandle.dylib candle.so
-	PYTHONPATH=.:$PYTHONPATH python3 candle-pyo3/test.py
-
-.PHONY: pyo3-test pyo3-test-macos clean-ptx clean test
+.PHONY: clean-ptx clean test
 
 clean-ptx:
 	find target -name "*.ptx" -type f -delete
--- candle-pyo3/test.py
+++ candle-pyo3/test.py
@@ -1,18 +1,3 @@
-import os
-import sys
-
-# The "import candle" statement below works if there is a "candle.so" file in sys.path.
-# Here we check for shared libraries that can be used in the build directory.
-BUILD_DIR = "./target/release-with-debug"
-so_file = BUILD_DIR + "/candle.so"
-if os.path.islink(so_file): os.remove(so_file)
-for lib_file in ["libcandle.dylib", "libcandle.so"]:
-    lib_file_ = BUILD_DIR + "/" + lib_file
-    if os.path.isfile(lib_file_):
-        os.symlink(lib_file, so_file)
-        sys.path.insert(0, BUILD_DIR)
-        break
-
 import candle
 
 t = candle.Tensor(42.0)
