a03e93aa0f4c86557deef382b3359677b038ac7e
Laurent <laurent.mazare@gmail.com>
2023-09-07 10:19:48 +0200

    Sketch the prompt encoder.

--- candle-examples/examples/segment-anything/model_prompt_encoder.rs
+++ candle-examples/examples/segment-anything/model_prompt_encoder.rs
@@ -50,4 +50,69 @@ impl PostionEmbeddingRandom {
 }
 
 #[derive(Debug)]
-struct PromptEncoder {}
+struct PromptEncoder {
+    pe_layer: PostionEmbeddingRandom,
+    point_embeddings: Vec<candle_nn::Embedding>,
+    not_a_point_embed: candle_nn::Embedding,
+    mask_downscaling_conv1: candle_nn::Conv2d,
+    mask_downscaling_ln1: LayerNorm,
+    mask_downscaling_conv2: candle_nn::Conv2d,
+    mask_downscaling_ln2: LayerNorm,
+    mask_downscaling_conv3: candle_nn::Conv2d,
+    no_mask_embed: candle_nn::Embedding,
+}
+
+impl PromptEncoder {
+    fn new(
+        embed_dim: usize,
+        image_embedding_size: (usize, usize),
+        input_image_size: (usize, usize),
+        mask_in_chans: usize,
+        vb: VarBuilder,
+    ) -> Result<Self> {
+        let num_points_embeddings = 4;
+        let pe_layer = PostionEmbeddingRandom::new(embed_dim / 2, vb.pp("pe_layer"))?;
+        let not_a_point_embed = candle_nn::embedding(1, embed_dim, vb.pp("not_a_point_embed"))?;
+        let no_mask_embed = candle_nn::embedding(1, embed_dim, vb.pp("no_mask_embed"))?;
+        let cfg = candle_nn::Conv2dConfig {
+            stride: 2,
+            ..Default::default()
+        };
+        let mask_downscaling_conv1 =
+            candle_nn::conv2d(1, mask_in_chans / 4, 2, cfg, vb.pp("mask_downscaling.0"))?;
+        let mask_downscaling_conv2 = candle_nn::conv2d(
+            mask_in_chans / 4,
+            mask_in_chans,
+            2,
+            cfg,
+            vb.pp("mask_downscaling.3"),
+        )?;
+        let mask_downscaling_conv3 = candle_nn::conv2d(
+            mask_in_chans,
+            embed_dim,
+            1,
+            Default::default(),
+            vb.pp("mask_downscaling.6"),
+        )?;
+        let mask_downscaling_ln1 =
+            layer_norm(mask_in_chans / 4, 1e-6, vb.pp("mask_downscaling.1"))?;
+        let mask_downscaling_ln2 = layer_norm(mask_in_chans, 1e-6, vb.pp("mask_downscaling.4"))?;
+        let mut point_embeddings = Vec::with_capacity(num_points_embeddings);
+        let vb_e = vb.pp("points_embeddings");
+        for i in 0..num_points_embeddings {
+            let emb = candle_nn::embedding(1, embed_dim, vb_e.pp(i))?;
+            point_embeddings.push(emb)
+        }
+        Ok(Self {
+            pe_layer,
+            point_embeddings,
+            not_a_point_embed,
+            mask_downscaling_conv1,
+            mask_downscaling_ln1,
+            mask_downscaling_conv2,
+            mask_downscaling_ln2,
+            mask_downscaling_conv3,
+            no_mask_embed,
+        })
+    }
+}
