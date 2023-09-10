e05a7dbea4964a2cec81f69429635cc2e410bf7b
Laurent <laurent.mazare@gmail.com>
2023-09-10 22:32:03 +0100

    Handle non-contiguous kernels.

--- candle-core/src/cpu_backend.rs
+++ candle-core/src/cpu_backend.rs
@@ -2312,9 +2312,7 @@ impl BackendStorage for CpuStorage {
         kernel_l: &Layout,
         params: &crate::conv::ParamsConv2D,
     ) -> Result<Self> {
-        // println!("{kernel_l:?}");
         // return Conv2D(params).map(self, l, kernel, kernel_l);
-        // TODO: handle kernel_l
         let op = Im2Col {
             h_k: params.k_h,
             w_k: params.k_w,
@@ -2329,10 +2327,20 @@ impl BackendStorage for CpuStorage {
         let k = op.h_k * op.w_k * params.c_in;
         let m = h_out * w_out;
         let col_l = Layout::contiguous((b, m, k));
-        let kernel_l = Layout::contiguous_with_offset((1, n, k), kernel_l.start_offset())
-            .transpose(1, 2)?
-            .broadcast_as((b, k, n))?;
-        let res = col.matmul(kernel, (b, m, n, k), &col_l, &kernel_l)?;
+        let res = if kernel_l.is_contiguous() {
+            let kernel_l = Layout::contiguous_with_offset((1, n, k), kernel_l.start_offset())
+                .transpose(1, 2)?
+                .broadcast_as((b, k, n))?;
+            col.matmul(kernel, (b, m, n, k), &col_l, &kernel_l)?
+        } else {
+            // Make the kernel contiguous if not already the case.
+            let mut kernel_c = self.device().zeros_impl(kernel_l.shape(), kernel.dtype())?;
+            kernel.copy_strided_src(&mut kernel_c, 0, kernel_l)?;
+            let kernel_l = Layout::contiguous_with_offset((1, n, k), kernel_l.start_offset())
+                .transpose(1, 2)?
+                .broadcast_as((b, k, n))?;
+            col.matmul(kernel, (b, m, n, k), &col_l, &kernel_l)?
+        };
         let res_l = Layout::contiguous((b, h_out, w_out, params.c_out))
             .transpose(1, 2)?
             .transpose(1, 3)?;
