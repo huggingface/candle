7897acafa3dc8c1bd12cc7a8d7c8d7da987a0049
Juarez Bochi <juarez.bochi@grammarly.com>
2023-09-12 10:32:23 -0400

    Add tests

---
+++ candle-transformers/tests/generation_tests.rs
@@ -0,0 +1,29 @@
+use candle::{Device, Result, Tensor};
+use candle_transformers::generation::LogitsProcessor;
+
+#[test]
+fn sample_with_zero_temperature() -> Result<()> {
+    let mut logits_process = LogitsProcessor::new(1337, None, None);
+    let logits = Tensor::new(&[0.1, 0.2, 0.3, 0.4], &Device::Cpu)?;
+    let token = logits_process.sample(&logits)?;
+    assert_eq!(token, 3);
+    Ok(())
+}
+
+#[test]
+fn sample_with_temperature() -> Result<()> {
+    let mut logits_process = LogitsProcessor::new(42, Some(0.9), None);
+    let logits = Tensor::new(&[0.1, 0.2, 0.3, 0.4], &Device::Cpu)?;
+    let token = logits_process.sample(&logits)?;
+    assert_eq!(token, 0);
+    Ok(())
+}
+
+#[test]
+fn sample_with_top_p() -> Result<()> {
+    let mut logits_process = LogitsProcessor::new(42, Some(1.0), Some(0.5));
+    let logits = Tensor::new(&[0.1, 0.2, 0.3, 0.4], &Device::Cpu)?;
+    let token = logits_process.sample(&logits)?;
+    assert_eq!(token, 2);
+    Ok(())
+}
