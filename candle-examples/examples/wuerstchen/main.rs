f06bc5a8090f36bf34bde96b4264c5aa86866496
laurent <laurent.mazare@gmail.com>
2023-09-19 21:57:42 +0100

    Fix the picture denormalization.

--- candle-examples/examples/wuerstchen/main.rs
+++ candle-examples/examples/wuerstchen/main.rs
@@ -373,7 +373,6 @@ fn run(args: Args) -> Result<()> {
         );
         let image = vqgan.decode(&(&latents * 0.3764)?)?;
         // TODO: Add the clamping between 0 and 1.
-        let image = ((image / 2.)? + 0.5)?.to_device(&Device::Cpu)?;
         let image = (image * 255.)?.to_dtype(DType::U8)?.i(0)?;
         let image_filename = output_filename(&final_image, idx + 1, num_samples, None);
         candle_examples::save_image(&image, image_filename)?
