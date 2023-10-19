9261697f808e5b187b14905ef22c7da33546956e
Laurent <laurent.mazare@gmail.com>
2023-10-19 20:40:11 +0100

    Add self-attn.

--- candle-transformers/src/models/vit.rs
+++ candle-transformers/src/models/vit.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
-use candle::{Result, Tensor};
+use crate::models::with_tracing::{conv2d, linear, linear_no_bias, Conv2d, Linear};
+use candle::{Module, Result, Tensor, D};
 use candle_nn::VarBuilder;
 
 // https://github.com/huggingface/transformers/blob/main/src/transformers/models/vit/configuration_vit.py
@@ -36,7 +37,7 @@ impl Config {
 
 struct PatchEmbeddings {
     num_patches: usize,
-    projection: candle_nn::Conv2d,
+    projection: Conv2d,
 }
 
 impl PatchEmbeddings {
@@ -48,7 +49,7 @@ impl PatchEmbeddings {
             stride: patch_size,
             ..Default::default()
         };
-        let projection = candle_nn::conv2d(
+        let projection = conv2d(
             cfg.num_channels,
             cfg.hidden_size,
             patch_size,
@@ -60,6 +61,14 @@ impl PatchEmbeddings {
             projection,
         })
     }
+
+    fn forward(&self, pixel_values: &Tensor, interpolate_pos_encoding: bool) -> Result<Tensor> {
+        let (b_size, num_channels, height, width) = pixel_values.dims4()?;
+        self.projection
+            .forward(pixel_values)?
+            .flatten_from(2)?
+            .transpose(1, 2)
+    }
 }
 
 struct Embeddings {
@@ -67,25 +76,137 @@ struct Embeddings {
     mask_token: Option<Tensor>,
     patch_embeddings: PatchEmbeddings,
     position_embeddings: Tensor,
+    hidden_size: usize,
 }
 
 impl Embeddings {
     fn new(cfg: &Config, use_mask_token: bool, vb: VarBuilder) -> Result<Self> {
-        let cls_token = vb.get((1, 1, cfg.hidden_size), "cls_token")?;
+        let hidden_size = cfg.hidden_size;
+        let cls_token = vb.get((1, 1, hidden_size), "cls_token")?;
         let mask_token = if use_mask_token {
-            Some(vb.get((1, 1, cfg.hidden_size), "mask_token")?)
+            Some(vb.get((1, 1, hidden_size), "mask_token")?)
         } else {
             None
         };
         let patch_embeddings = PatchEmbeddings::new(cfg, vb.pp("patch_embeddings"))?;
         let num_patches = patch_embeddings.num_patches;
         let position_embeddings =
-            vb.get((1, num_patches + 1, cfg.hidden_size), "position_embeddings")?;
+            vb.get((1, num_patches + 1, hidden_size), "position_embeddings")?;
         Ok(Self {
             cls_token,
             mask_token,
             patch_embeddings,
             position_embeddings,
+            hidden_size,
         })
+    }
+
+    fn interpolate_pos_encoding(
+        &self,
+        embeddings: &Tensor,
+        height: usize,
+        width: usize,
+    ) -> Result<Tensor> {
+        todo!()
+    }
+
+    fn forward(
+        &self,
+        pixel_values: &Tensor,
+        bool_masked_pos: Option<&Tensor>,
+        interpolate_pos_encoding: bool,
+    ) -> Result<Tensor> {
+        let (b_size, num_channels, height, width) = pixel_values.dims4()?;
+        let embeddings = self
+            .patch_embeddings
+            .forward(pixel_values, interpolate_pos_encoding)?;
+        let embeddings = match (bool_masked_pos, &self.mask_token) {
+            (None, _) => embeddings,
+            (Some(_), None) => candle::bail!("bool_masked_pos set without mask_token"),
+            (Some(bool_masked_pos), Some(mask_tokens)) => {
+                let seq_len = embeddings.dim(1)?;
+                let mask_tokens = mask_tokens.broadcast_as((b_size, seq_len, self.hidden_size))?;
+                let mask = bool_masked_pos
+                    .unsqueeze(D::Minus1)?
+                    .to_dtype(mask_tokens.dtype())?;
+                ((mask_tokens * &mask)? - (embeddings * (mask - 1.)?)?)?
+            }
+        };
+        let cls_tokens = self.cls_token.broadcast_as((b_size, 1, self.hidden_size))?;
+        let embeddings = Tensor::cat(&[&cls_tokens, &embeddings], 1)?;
+        if interpolate_pos_encoding {
+            let pos = self.interpolate_pos_encoding(&embeddings, height, width)?;
+            embeddings.broadcast_add(&pos)
+        } else {
+            embeddings.broadcast_add(&self.position_embeddings)
+        }
+    }
+}
+
+struct SelfAttention {
+    query: Linear,
+    key: Linear,
+    value: Linear,
+    num_attention_heads: usize,
+    attention_head_size: usize,
+}
+
+impl SelfAttention {
+    fn new(cfg: &Config, vb: VarBuilder) -> Result<Self> {
+        let attention_head_size = cfg.hidden_size / cfg.num_attention_heads;
+        let num_attention_heads = cfg.num_attention_heads;
+        let all_head_size = num_attention_heads * attention_head_size;
+        let linear = |name| {
+            if cfg.qkv_bias {
+                linear(cfg.hidden_size, all_head_size, vb.pp(name))
+            } else {
+                linear_no_bias(cfg.hidden_size, all_head_size, vb.pp(name))
+            }
+        };
+        let query = linear("query")?;
+        let key = linear("key")?;
+        let value = linear("value")?;
+        Ok(Self {
+            query,
+            key,
+            value,
+            num_attention_heads,
+            attention_head_size,
+        })
+    }
+
+    fn transpose_for_scores(&self, xs: &Tensor) -> Result<Tensor> {
+        let (b_size, seq_len, _) = xs.dims3()?;
+        xs.reshape((
+            b_size,
+            seq_len,
+            self.num_attention_heads,
+            self.attention_head_size,
+        ))?
+        .permute((0, 2, 1, 3))
+    }
+
+    fn forward(
+        &self,
+        xs: &Tensor,
+        bool_masked_pos: Option<&Tensor>,
+        interpolate_pos_encoding: bool,
+    ) -> Result<Tensor> {
+        let query = self.query.forward(xs)?;
+        let key = self.key.forward(xs)?;
+        let value = self.value.forward(xs)?;
+
+        let query = self.transpose_for_scores(&query)?;
+        let key = self.transpose_for_scores(&key)?;
+        let value = self.transpose_for_scores(&value)?;
+
+        let attention_scores =
+            (query.matmul(&key.t()?)? / f64::sqrt(self.attention_head_size as f64))?;
+        let attention_probs = candle_nn::ops::softmax_last_dim(&attention_scores)?;
+        attention_probs
+            .matmul(&value)?
+            .permute((0, 2, 1, 3))?
+            .contiguous()?
+            .flatten_from(D::Minus2)
     }
 }
