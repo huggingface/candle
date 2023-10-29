d280b163df8288ef1c15f9ee8298a5971bafcbbe
Laurent <laurent.mazare@gmail.com>
2023-10-29 13:04:01 +0100

    Implement the attention forward method.

--- candle-transformers/src/models/marian.rs
+++ candle-transformers/src/models/marian.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 use super::with_tracing::{linear, linear_no_bias, Embedding, Linear};
-use candle::{Result, Tensor};
+use candle::{Module, Result, Tensor};
 use candle_nn::{layer_norm, LayerNorm, VarBuilder};
 
 #[derive(Debug, Clone)]
@@ -53,6 +53,17 @@ impl SinusoidalPositionalEmbedding {
         let emb = Embedding::from_weights(weights)?;
         Ok(Self { emb })
     }
+
+    fn forward(&self, input_ids: &Tensor, past_kv_len: usize) -> Result<Tensor> {
+        let b_size = input_ids.dim(0)?;
+        let seq_len = input_ids.dim(1)?;
+        Tensor::arange(
+            past_kv_len as u32,
+            (past_kv_len + seq_len) as u32,
+            input_ids.device(),
+        )?
+        .apply(&self.emb)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -61,16 +72,21 @@ struct Attention {
     k_proj: Linear,
     v_proj: Linear,
     out_proj: Linear,
+    scaling: f64,
+    num_heads: usize,
+    head_dim: usize,
 }
 
 impl Attention {
     fn new(cfg: &Config, is_decoder: bool, vb: VarBuilder) -> Result<Self> {
-        let _attention_heads = if is_decoder {
+        let num_heads = if is_decoder {
             cfg.decoder_attention_heads
         } else {
             cfg.encoder_attention_heads
         };
         let embed_dim = cfg.d_model;
+        let head_dim = embed_dim / num_heads;
+        let scaling = (head_dim as f64).powf(-0.5);
         let q_proj = linear(embed_dim, embed_dim, vb.pp("q_proj"))?;
         let k_proj = linear(embed_dim, embed_dim, vb.pp("k_proj"))?;
         let v_proj = linear(embed_dim, embed_dim, vb.pp("v_proj"))?;
@@ -80,7 +96,48 @@ impl Attention {
             k_proj,
             v_proj,
             out_proj,
+            scaling,
+            num_heads,
+            head_dim,
         })
+    }
+
+    fn _shape(&self, tensor: &Tensor, bsz: usize) -> Result<Tensor> {
+        tensor
+            .reshape((bsz, (), self.num_heads, self.head_dim))?
+            .transpose(1, 2)?
+            .contiguous()
+    }
+
+    fn forward(&self, xs: &Tensor, kv_states: Option<&Tensor>) -> Result<Tensor> {
+        let is_cross_attn = kv_states.is_some();
+        let (b_sz, tgt_len, _) = xs.dims3()?;
+        let query_states = (xs.apply(&self.q_proj)? * self.scaling)?;
+        let (key_states, value_states) = match kv_states {
+            None => {
+                let key_states = self._shape(&xs.apply(&self.k_proj)?, b_sz)?;
+                let value_states = self._shape(&xs.apply(&self.v_proj)?, b_sz)?;
+                (key_states, value_states)
+            }
+            Some(kv_states) => {
+                let key_states = self._shape(&kv_states.apply(&self.k_proj)?, b_sz)?;
+                let value_states = self._shape(&kv_states.apply(&self.v_proj)?, b_sz)?;
+                (key_states, value_states)
+            }
+        };
+        let proj_shape = (b_sz * self.num_heads, (), self.head_dim);
+        let query_states = self._shape(&query_states, b_sz)?.reshape(proj_shape)?;
+        let key_states = key_states.reshape(proj_shape)?;
+        let value_states = value_states.reshape(proj_shape)?;
+        let attn_weights = query_states.matmul(&key_states.transpose(1, 2)?)?;
+        // todo: attn_mask
+        let attn_probs = candle_nn::ops::softmax_last_dim(&attn_weights)?;
+        let attn_output = attn_probs.matmul(&value_states)?;
+        attn_output
+            .reshape((b_sz, self.num_heads, tgt_len, self.head_dim))?
+            .transpose(1, 2)?
+            .reshape((b_sz, tgt_len, self.head_dim * self.num_heads))?
+            .apply(&self.out_proj)
     }
 }
 
