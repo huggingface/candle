cc55fa289ab26969929561b830577a1da17cab44
Laurent <laurent.mazare@gmail.com>
2023-11-03 22:15:33 +0100

    Use clap for the onnx tool.

--- candle-onnx/examples/onnx_basics.rs
+++ candle-onnx/examples/onnx_basics.rs
@@ -1,7 +1,33 @@
 use anyhow::Result;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand, Debug, Clone)]
+enum Command {
+    Print {
+        #[arg(long)]
+        file: String,
+    },
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
 pub fn main() -> Result<()> {
-    let v = candle_onnx::read_file("test.onnx")?;
-    println!("{v:?}");
+    let args = Args::parse();
+    match args.command {
+        Command::Print { file } => {
+            let model = candle_onnx::read_file(file)?;
+            println!("{model:?}");
+            let graph = model.graph.unwrap();
+            for node in graph.node.iter() {
+                println!("{node:?}");
+            }
+        }
+    }
     Ok(())
 }
