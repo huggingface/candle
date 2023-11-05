a807cc6cf5e9e92d68bcacf2a41e3ae037ce35aa
EricLBuehler <ericlbuehler@gmail.com>
2023-11-04 20:47:18 -0400

    Fix clippy

--- candle-transformers/tests/generation_tests.rs
+++ candle-transformers/tests/generation_tests.rs
@@ -1,9 +1,9 @@
 use candle::{Device, Result, Tensor};
-use candle_transformers::generation::LogitsProcessor;
+use candle_transformers::generation::{LogitsProcessor, SamplingMethod};
 
 #[test]
 fn sample_with_zero_temperature() -> Result<()> {
-    let mut logits_process = LogitsProcessor::new(1337, None, None);
+    let mut logits_process = LogitsProcessor::new(1337, None, SamplingMethod::Argmax);
     let logits = Tensor::new(&[0.1, 0.2, 0.3, 0.4], &Device::Cpu)?;
     let token = logits_process.sample(&logits)?;
     assert_eq!(token, 3);
@@ -12,7 +12,7 @@ fn sample_with_zero_temperature() -> Result<()> {
 
 #[test]
 fn sample_with_temperature() -> Result<()> {
-    let mut logits_process = LogitsProcessor::new(42, Some(0.9), None);
+    let mut logits_process = LogitsProcessor::new(42, Some(0.9), SamplingMethod::Argmax);
     let logits = Tensor::new(&[0.1, 0.2, 0.3, 0.4], &Device::Cpu)?;
     let token = logits_process.sample(&logits)?;
     assert_eq!(token, 0);
@@ -21,7 +21,7 @@ fn sample_with_temperature() -> Result<()> {
 
 #[test]
 fn sample_with_top_p() -> Result<()> {
-    let mut logits_process = LogitsProcessor::new(42, Some(1.0), Some(0.5));
+    let mut logits_process = LogitsProcessor::new(42, Some(1.0), SamplingMethod::TopP(0.5));
     let logits = Tensor::new(&[0.1, 0.2, 0.3, 0.4], &Device::Cpu)?;
     let token = logits_process.sample(&logits)?;
     assert_eq!(token, 2);
