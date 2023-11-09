29a5110fbb051465841634f1a8a893081043832e
Nicolas Patry <patry.nicolas@protonmail.com>
2023-11-09 16:50:15 +0100

    Fmt.

--- candle-examples/src/lib.rs
+++ candle-examples/src/lib.rs
@@ -15,13 +15,13 @@ pub fn device(cpu: bool) -> Result<Device> {
     } else {
         #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
         {
-            println!("Running on CPU, to run on GPU(metal), build this example with `--features metal`");
+            println!(
+                "Running on CPU, to run on GPU(metal), build this example with `--features metal`"
+            );
         }
         #[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
         {
-            println!(
-                "Running on CPU, to run on GPU, build this example with `--features cuda`"
-            );
+            println!("Running on CPU, to run on GPU, build this example with `--features cuda`");
         }
         Ok(Device::Cpu)
     }
