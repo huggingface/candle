0ccd8e100885ddcd932b1888235340e724705513
laurent <laurent.mazare@gmail.com>
2023-11-23 07:33:17 +0000

    Handle steps == 0 and steps == 1.

--- candle-transformers/src/models/stable_diffusion/utils.rs
+++ candle-transformers/src/models/stable_diffusion/utils.rs
@@ -1,12 +1,15 @@
 use candle::{Device, Result, Tensor};
 
 pub fn linspace(start: f64, stop: f64, steps: usize) -> Result<Tensor> {
-    if steps <= 1 {
-        candle::bail!("cannot use linspace with steps {steps} <= 1")
+    if steps == 0 {
+        Tensor::from_vec(Vec::<f64>::new(), steps, &Device::Cpu)
+    } else if steps == 1 {
+        Tensor::from_vec(vec![start], steps, &Device::Cpu)
+    } else {
+        let delta = (stop - start) / (steps - 1) as f64;
+        let vs = (0..steps)
+            .map(|step| start + step as f64 * delta)
+            .collect::<Vec<_>>();
+        Tensor::from_vec(vs, steps, &Device::Cpu)
     }
-    let delta = (stop - start) / (steps - 1) as f64;
-    let vs = (0..steps)
-        .map(|step| start + step as f64 * delta)
-        .collect::<Vec<_>>();
-    Tensor::from_vec(vs, steps, &Device::Cpu)
 }
