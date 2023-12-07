f704aba8950e1a74d7c97197d76d348e32bccbc1
jeromeku <jerome.ku@gmail.com>
2023-12-06 23:44:48 -0800

    add cuda impl

---
+++ candle-examples/examples/search-sorted/search_kernel/search_sorted.cu
@@ -0,0 +1,106 @@
+
+#include <cuda_runtime.h>
+#include <cuda.h>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include "search_sorted_kernel.cu"
+
+template <typename input_t, typename output_t>
+extern __global__ void searchsorted_cuda_kernel(
+    output_t *data_out,
+    const input_t *data_in,
+    const input_t *data_bd,
+    int idim_in,
+    int idim_bd,
+    int numel_in,
+    bool right,
+    bool is_1d_boundaries,
+    bool is_1d_values);
+
+inline int getMaxThreadsPerBlock()
+{
+    int device;
+    cudaDeviceProp props;
+    cudaGetDevice(&device);
+    cudaGetDeviceProperties(&props, device);
+    return props.maxThreadsPerBlock;
+}
+
+template <typename input_t, typename output_t>
+std::vector<output_t> search_sorted(std::vector<input_t> sorted_seq, int innerdim_ss, std::vector<input_t> values, int innerdim_vs, bool right)
+{
+
+    assert(values.size() % innerdim_vs == 0);
+    assert(sorted_seq.size() % innerdim_ss == 0);
+
+    int num_val_rows = values.size() / innerdim_vs;
+    int num_seq_rows = sorted_seq.size() / innerdim_ss;
+
+    bool is_1d_boundaries = num_seq_rows == 1 ? 1 : 0;
+    bool is_1d_values = num_val_rows == 1 ? 1 : 0;
+    int num_output_rows = std::max(num_val_rows, num_seq_rows);
+
+    std::vector<output_t> output(num_output_rows * innerdim_vs);
+    std::fill(output.begin(), output.end(), 0);
+    int numel = output.size();
+    // Allocate device memory
+    input_t *d_tensor;
+    input_t *d_values;
+    int *d_output;
+    cudaMalloc(&d_tensor, sorted_seq.size() * sizeof(input_t));
+    cudaMalloc(&d_values, values.size() * sizeof(input_t));
+    cudaMalloc(&d_output, output.size() * sizeof(output_t));
+
+    // Copy data to device
+    cudaMemcpy(d_tensor, sorted_seq.data(), sorted_seq.size() * sizeof(input_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_values, values.data(), values.size() * sizeof(input_t), cudaMemcpyHostToDevice);
+
+    // Launch kernel
+    int maxThreads = getMaxThreadsPerBlock();
+    int maxGrid = 1024;
+    dim3 block = dim3(std::min(maxThreads, numel));
+    dim3 grid = dim3(std::min(maxGrid, int((numel + block.x - 1) / block.x)));
+
+    // Innermost dimensions: for values, this corresponds to the number of values to search for per row
+    searchsorted_cuda_kernel<<<grid, block>>>(d_output, d_values, d_tensor, innerdim_vs, innerdim_ss, numel, right, is_1d_boundaries, is_1d_values);
+    cudaDeviceSynchronize();
+    // Copy output data back to host
+    cudaMemcpy(output.data(), d_output, output.size() * sizeof(output_t), cudaMemcpyDeviceToHost);
+    // Free device memory
+    cudaFree(d_tensor);
+    cudaFree(d_values);
+    cudaFree(d_output);
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v)
+{
+    os << "[";
+    for (int i = 0; i < v.size(); i++)
+    {
+        os << v[i];
+        if (i != v.size() - 1)
+        {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
+
+int main()
+{
+
+    std::vector<float> ss = {1, 3, 5, 7, 9};
+    std::vector<float> vals = {3, 6, 9};
+    size_t innerdim_ss = 5;
+    size_t innerdim_vs = 3;
+    // ss_2d should be combined ss1_d and ss_1d_2
+
+    // Test 1-D ss, 1-D values
+    std::vector<int> output = search_sorted<float, int>(ss, innerdim_ss, vals, innerdim_vs, false);
+    std::cout << "Test 1-D ss: " << ss << "1-D values: " << vals << "\noutput: " << output << std::endl;
+    return 0;
+}
---
+++ candle-examples/examples/search-sorted/search_kernel/search_sorted_kernel.cu
@@ -0,0 +1,85 @@
+
+template <typename input_t>
+__device__ int lower_bound(const input_t *data_ss, int64_t start, int64_t end, const input_t val)
+{
+    // sorter gives relative ordering for ND tensors, so we need to save and add the non-updated start as an offset
+    // i.e. the second row of a 3x3 tensors starts at element 3 but sorter's second row only contains 0, 1, or 2
+    const int64_t orig_start = start;
+    while (start < end)
+    {
+        const int64_t mid = start + ((end - start) >> 1);
+        const input_t mid_val = data_ss[mid];
+        if (!(mid_val >= val))
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid;
+        }
+    }
+    return start;
+}
+
+template <typename input_t>
+__device__ int upper_bound(const input_t *data_ss, int64_t start, int64_t end, const input_t val)
+{
+    // sorter gives relative ordering for ND tensors, so we need to save and add the non-updated start as an offset
+    // i.e. the second row of a 3x3 tensors starts at element 3 but sorter's second row only contains 0, 1, or 2
+    const int orig_start = start;
+    while (start < end)
+    {
+        const int mid = start + ((end - start) >> 1);
+        const input_t mid_val = data_ss[mid];
+        if (!(mid_val > val))
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid;
+        }
+    }
+    // if (threadIdx.x >= 3)
+    //     printf("Thread id: %d, start: %d", threadIdx.x, start);
+    return start;
+}
+
+template <typename input_t, typename output_t>
+__global__ void searchsorted_cuda_kernel(
+    output_t *data_out,
+    const input_t *data_in,
+    const input_t *data_bd,
+    int idim_in,
+    int idim_bd,
+    int numel_in,
+    bool right,
+    bool is_1d_boundaries,
+    bool is_1d_values)
+{
+    // Define boundaries for sorted seq and values
+    for (int tid = blockIdx.x * blockDim.x + threadIdx.x; tid < numel_in; tid += blockDim.x * gridDim.x)
+    {
+        // If boundaries tensor is 1d, we always search the entire boundary tensor
+        int start_bd = is_1d_boundaries ? 0 : tid / idim_in * idim_bd;
+        int end_bd = start_bd + idim_bd;
+        int val_idx = is_1d_values ? tid % idim_in : tid;
+
+        // printf("Thread id: %d, start_bd: %d, end_bd: %d val_idx: %d, val: %f, idim_in: %d idim_bd: %d\n", tid, start_bd, end_bd, val_idx, data_in[val_idx], idim_in, idim_bd);
+        int pos = !right ? lower_bound<input_t>(data_bd, start_bd, end_bd, data_in[val_idx]) - start_bd : upper_bound<input_t>(data_bd, start_bd, end_bd, data_in[val_idx]) - start_bd;
+
+        data_out[tid] = pos;
+    }
+}
+
+// template <>
+// __global__ void searchsorted_cuda_kernel<float, int>(
+//     int *data_out,
+//     const float *data_in,
+//     const float *data_bd,
+//     int idim_in,
+//     int idim_bd,
+//     int numel_in,
+//     bool right,
+//     bool is_1d_boundaries,
+//     bool is_1d_values);
