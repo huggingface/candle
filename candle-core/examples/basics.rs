bab3f973dbf6109403d497f3025e62d358c369aa
Laurent <laurent.mazare@gmail.com>
2023-12-23 11:11:50 +0100

    Validate the kernel size in pooling ops.

--- candle-core/examples/basics.rs
+++ candle-core/examples/basics.rs
@@ -4,14 +4,13 @@ extern crate intel_mkl_src;
 #[cfg(feature = "accelerate")]
 extern crate accelerate_src;
 
-use anyhow::Result;
-use candle_core::{Device, Tensor};
+use candle_core::{Device, Result, Tensor};
 
 fn main() -> Result<()> {
-    let a = Tensor::new(&[[0.0f32, 1.0, 2.0], [3.0, 4.0, 5.0]], &Device::Cpu)?;
-    let b = Tensor::new(&[[88.0f32, 99.0]], &Device::Cpu)?;
-    let new_a = a.slice_scatter(&b, 1, 2)?;
-    assert_eq!(a.to_vec2::<f32>()?, [[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]]);
-    assert_eq!(new_a.to_vec2::<f32>()?, [[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]]);
+    let xs = Tensor::arange::<f32>(0., 64., &Device::Cpu)?.reshape((1, 64, 1, 1))?;
+
+    let xs = xs.max_pool2d(2)?;
+    println!("{}", xs);
+
     Ok(())
 }
