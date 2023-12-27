cc4cd74b6f057be7d345cd6931d43b4dd1fd6642
Ryan Tate <ryan.michael.tate@gmail.com>
2023-12-27 13:32:07 -0800

    one-hot: use two generics, one_hot::<I, O>, for input and output data types
    Separating the input and output data types allows the input tensor
    indices to be a different data type than the output encoded tensor data type.
    
    For example, one_hot::<i64, u8>(...) will take an input tensor of i64 values
    and encode the output tensor using u8 values.
    
    The generic I::DTYPE must match the data type of the input indices, otherwise
    the method will bail.
    
    Additionally, this method adds an `allow_f64` option to enable the input indices
    data type to be f64 values. f64 values are disabled by default.
    
    TODO: indices data type and the generic I data type are currently not compile-time
    checked.

--- candle-nn/src/encoding.rs
+++ candle-nn/src/encoding.rs
@@ -8,21 +8,50 @@
 //! use candle::{Shape, Tensor, Device};
 //! use candle_nn::encoding::one_hot;
 //!
-//! let device = Device::Cpu;
+//! let device = candle::Device::Cpu;
+//!
+//! let indices = Tensor::new(vec![vec![0f32, 2.], vec![1., -1.]], &device)?;
 //! let depth = 4;
-//! let indices = Tensor::new(vec![vec![0f32, 2.], vec![1., -1.]], &device).unwrap();
 //!
-//! let on_value = Some(1.0); // default
-//! let off_value = Some(0.0); // default
+//! let allow_f64 = true;
+//! let on_value = None; // defaults to 1.0
+//! let off_value = None; // defaults to 0.0
 //!
-//! let one_hot = one_hot::<f32>(indices, depth, on_value, off_value).unwrap();
+//! let one_hot = one_hot::<f32, f32>(indices, depth, allow_f64, on_value, off_value)?;
+//!
 //! let expected_matrix = [
 //!     [[1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]],
 //!     [[0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]],
 //! ];
-//! assert_eq!(one_hot.shape(), &Shape::from((2, 2, 4)));
 //!
-//! let matrix = one_hot.to_vec3::<f32>().unwrap();
+//! assert_eq!(one_hot.shape(), &Shape::from((2, 2, depth)));
+//!
+//! let matrix = one_hot.to_vec3::<f32>()?;
+//!
+//! assert_eq!(matrix, expected_matrix);
+//!```
+//! ## One-cold Encoding
+//!
+//! ```rust
+//! use candle::{Shape, Tensor, Device};
+//! use candle_nn::encoding::one_hot;
+//!
+//!
+//! let device = candle::Device::Cpu;
+//! let depth = 4;
+//! let indices = Tensor::new(vec![vec![0i64, 2], vec![1, 3]], &device)?;
+//!
+//! let allow_f64 = false;
+//! let on_value = Some(0);
+//! let off_value = Some(1);
+//!
+//! let one_cold = one_hot::<i64, u8>(indices, depth, allow_f64, on_value, off_value)?;
+//!
+//! let expected_matrix = [[[0, 1, 1, 1], [1, 1, 0, 1]], [[1, 0, 1, 1], [1, 1, 1, 0]]];
+//!
+//! assert_eq!(one_cold.shape(), &Shape::from((2, 2, depth)));
+//!
+//! let matrix = one_cold.to_vec3::<u8>()?;
 //!
 //! assert_eq!(matrix, expected_matrix);
 //! ```
@@ -31,11 +60,11 @@
 use candle::{bail, Result, Tensor, WithDType};
 
 const INVALID_ONE_HOT_INDEX_MSG: &str =
-    "Invalid negative index value. Expected a positive index value or `-1` value to ignore.";
+    "one_hot: Invalid negative index value. Expected a positive index value or `-1` value to ignore.";
 
-const INDEX_OUT_OF_BOUNDS_MSG: &str = "Index out of bounds.";
+const INDEX_OUT_OF_BOUNDS_MSG: &str = "one_hot: Index out of bounds.";
 
-const INDEX_EXCEEDS_DEPTH_MSG: &str = "Index value exceeds the depth value.";
+const INDEX_EXCEEDS_DEPTH_MSG: &str = "one_hot: Index value exceeds the depth value.";
 
 /// One-hot/cold encoding.
 ///
@@ -68,60 +97,106 @@ const INDEX_EXCEEDS_DEPTH_MSG: &str = "Index value exceeds the depth value.";
 ///
 /// # Examples
 ///
-///```rust
+/// ## One-hot encoding
+///
+/// ```rust
 /// use candle::{Shape, Tensor, Device};
 /// use candle_nn::encoding::one_hot;
 ///
-/// let device = Device::Cpu;
+/// let device = candle::Device::Cpu;
+///
+/// let indices = Tensor::new(vec![vec![0f32, 2.], vec![1., -1.]], &device)?;
 /// let depth = 4;
-/// let indices = Tensor::new(vec![vec![0f32, 2.], vec![1., -1.]], &device).unwrap();
 ///
-/// let on_value = Some(1.0); // default
-/// let off_value = Some(0.0); // default
+/// let allow_f64 = true;
+/// let on_value = None; // defaults to 1.0
+/// let off_value = None; // defaults to 0.0
 ///
-/// let one_hot = one_hot::<f32>(indices, depth, on_value, off_value).unwrap();
+/// let one_hot = one_hot::<f32, f32>(indices, depth, allow_f64, on_value, off_value)?;
+///
 /// let expected_matrix = [
 ///     [[1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]],
 ///     [[0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]],
 /// ];
-/// assert_eq!(one_hot.shape(), &Shape::from((2, 2, 4)));
 ///
-/// let matrix = one_hot.to_vec3::<f32>().unwrap();
+/// assert_eq!(one_hot.shape(), &Shape::from((2, 2, depth)));
+///
+/// let matrix = one_hot.to_vec3::<f32>()?;
+///
+/// assert_eq!(matrix, expected_matrix);
+///```
+/// ## One-cold Encoding
+///
+/// ```rust
+/// use candle::{Shape, Tensor, Device};
+/// use candle_nn::encoding::one_hot;
+///
+///
+/// let device = candle::Device::Cpu;
+/// let depth = 4;
+/// let indices = Tensor::new(vec![vec![0i64, 2], vec![1, 3]], &device)?;
+///
+/// let allow_f64 = false;
+/// let on_value = Some(0);
+/// let off_value = Some(1);
+///
+/// let one_cold = one_hot::<i64, u8>(indices, depth, allow_f64, on_value, off_value)?;
+///
+/// let expected_matrix = [[[0, 1, 1, 1], [1, 1, 0, 1]], [[1, 0, 1, 1], [1, 1, 1, 0]]];
+///
+/// assert_eq!(one_cold.shape(), &Shape::from((2, 2, depth)));
+///
+/// let matrix = one_cold.to_vec3::<u8>()?;
 ///
 /// assert_eq!(matrix, expected_matrix);
 /// ```
+///
 ///
 /// # Bails
 ///
 /// This method will bail on tensors with a rank greater than 3.
 ///
+/// This method will bail if the index value is less than -1.
+///
+/// This method will bail if the index value is greater than or equal to the depth value.
+///
+/// This method will bail if the input data type does not equal the generic data type provided.
+///
+/// This method will bail if the input data type is `f64` and `allow_f64` is false.
+///
 /// # API Design
 ///
 /// The api design for this method is loosely based on the [TensorFlow One-Hot](https://www.tensorflow.org/api_docs/python/tf/one_hot) method.
-pub fn one_hot<D: WithDType>(
+pub fn one_hot<I: WithDType, O: WithDType>(
     indices: Tensor,
     depth: usize,
-    on_value: Option<D>,
-    off_value: Option<D>,
+    allow_f64: bool,
+    on_value: Option<O>,
+    off_value: Option<O>,
 ) -> Result<Tensor> {
-    let on_value = on_value.unwrap_or(D::from_f64(1.));
-    let off_value = off_value.unwrap_or(D::from_f64(0.));
+    let on_value = on_value.unwrap_or(O::from_f64(1.));
+    let off_value = off_value.unwrap_or(O::from_f64(0.));
+
+    if I::DTYPE != indices.dtype() {
+        bail!(
+            "one_hot: indices dtype {} does not match expected dtype {}",
+            indices.dtype().as_str(),
+            I::DTYPE.as_str()
+        )
+    }
+
+    if !allow_f64 && I::DTYPE.as_str() == "f64" {
+        bail!("one_hot: f64 is not supported")
+    }
 
     let rank = indices.rank();
     match rank {
         0 => {
             let mut v = vec![off_value; depth];
-            let index = indices.to_vec0::<D>()?;
+            let index = indices.to_vec0::<I>()?;
             let vi = index.to_f64();
 
-            if vi as usize >= depth {
-                bail!(
-                    "{} Index value, {}, exceeds the depth value, {}.",
-                    INDEX_EXCEEDS_DEPTH_MSG,
-                    vi,
-                    depth
-                )
-            }
+            check_value(vi, depth)?;
 
             if vi >= 0. {
                 v[vi as usize] = on_value;
@@ -132,23 +207,15 @@ pub fn one_hot<D: WithDType>(
         1 => {
             let dim1 = indices.dims1()?;
             let mut v = vec![off_value; depth * dim1];
-            let index = indices.to_vec1::<D>()?;
+            let index = indices.to_vec1::<I>()?;
             for i in 0..dim1 {
                 let vi = index[i].to_f64();
 
                 if vi == -1. {
-                    // Ignore -1 indicies, leave the vector as all off values
                     continue;
-                } else if vi < -1. {
-                    bail!("{}. Received {}", INVALID_ONE_HOT_INDEX_MSG, vi);
-                } else if vi as usize >= depth {
-                    bail!(
-                        "{} Index value, {}, exceeds the depth value, {}.",
-                        INDEX_EXCEEDS_DEPTH_MSG,
-                        vi,
-                        depth
-                    )
                 }
+
+                check_value(vi, depth)?;
 
                 let idx = i * depth + vi as usize;
 
@@ -169,24 +236,16 @@ pub fn one_hot<D: WithDType>(
         2 => {
             let (dim1, dim2) = indices.dims2()?;
             let mut v = vec![off_value; depth * dim1 * dim2];
-            let index = indices.to_vec2::<D>()?;
+            let index = indices.to_vec2::<I>()?;
             for i in 0..dim1 {
                 for j in 0..dim2 {
                     let vij = index[i][j].to_f64();
 
                     if vij == -1. {
-                        // Ignore -1 indicies, leave the vector as all off values
                         continue;
-                    } else if vij < -1. {
-                        bail!("{}. Received {}", INVALID_ONE_HOT_INDEX_MSG, vij);
-                    } else if vij as usize >= depth {
-                        bail!(
-                            "{} Index value, {}, exceeds the depth value, {}.",
-                            INDEX_EXCEEDS_DEPTH_MSG,
-                            vij,
-                            depth
-                        )
                     }
+
+                    check_value(vij, depth)?;
 
                     let idx = i * depth * dim2 + j * depth + vij as usize;
 
@@ -208,25 +267,17 @@ pub fn one_hot<D: WithDType>(
         3 => {
             let (dim1, dim2, dim3) = indices.dims3()?;
             let mut v = vec![off_value; depth * dim1 * dim2 * dim3];
-            let index = indices.to_vec3::<D>()?;
+            let index = indices.to_vec3::<I>()?;
             for i in 0..dim1 {
                 for j in 0..dim2 {
                     for k in 0..dim3 {
                         let vijk = index[i][j][k].to_f64();
 
                         if vijk == -1. {
-                            // Ignore -1 indicies, leave the vector as all off values
                             continue;
-                        } else if vijk < -1. {
-                            bail!("{}. Received {}", INVALID_ONE_HOT_INDEX_MSG, vijk);
-                        } else if vijk as usize >= depth {
-                            bail!(
-                                "{} Index value, {}, exceeds the depth value, {}.",
-                                INDEX_EXCEEDS_DEPTH_MSG,
-                                vijk,
-                                depth
-                            )
                         }
+
+                        check_value(vijk, depth)?; // bail on invalid index value
 
                         let idx =
                             i * depth * dim2 * dim3 + j * depth * dim3 + k * depth + vijk as usize;
@@ -248,9 +299,26 @@ pub fn one_hot<D: WithDType>(
             Tensor::new(v, indices.device())?.reshape(&[dim1, dim2, dim3, depth])
         }
         _ => {
-            bail!("one_hot: rank {} is not supported", rank)
+            bail!("one_hot: rank {} is not supported. ", rank)
         }
     }
+}
+
+fn check_value(value: f64, depth: usize) -> Result<()> {
+    if value < -1. {
+        bail!("{}. Received {}", INVALID_ONE_HOT_INDEX_MSG, value);
+    }
+
+    if value as usize >= depth {
+        bail!(
+            "{} Index value, {}, exceeds the depth value, {}.",
+            INDEX_EXCEEDS_DEPTH_MSG,
+            value,
+            depth
+        )
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -262,17 +330,22 @@ mod tests {
     #[test]
     pub fn test_f64_one_hot() -> std::result::Result<(), Box<dyn std::error::Error>> {
         let device = candle::Device::Cpu;
-        let depth = 4;
-        let indices = Tensor::new(vec![vec![0f32, 2.], vec![1., -1.]], &device)?;
 
-        let one_hot = one_hot::<f32>(indices, depth, None, None)?;
+        let indices = Tensor::new(vec![vec![0f32, 2.], vec![1., -1.]], &device)?;
+        let depth = 4;
+
+        let allow_f64 = true;
+        let on_value = None; // defaults to 1.0
+        let off_value = None; // defaults to 0.0
+
+        let one_hot = one_hot::<f32, f32>(indices, depth, allow_f64, on_value, off_value)?;
 
         let expected_matrix = [
             [[1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]],
             [[0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]],
         ];
 
-        assert_eq!(one_hot.shape(), &Shape::from((2, 2, 4)));
+        assert_eq!(one_hot.shape(), &Shape::from((2, 2, depth)));
 
         let matrix = one_hot.to_vec3::<f32>()?;
 
@@ -282,21 +355,22 @@ mod tests {
     }
 
     #[test]
-    pub fn test_u8_one_hot() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    pub fn test_u8_one_cold() -> std::result::Result<(), Box<dyn std::error::Error>> {
         let device = candle::Device::Cpu;
         let depth = 4;
-        let indices = Tensor::new(vec![vec![0u8, 2], vec![1, 3]], &device)?;
+        let indices = Tensor::new(vec![vec![0i64, 2], vec![1, -1]], &device)?;
 
-        let on_value = Some(0u8);
+        let allow_f64 = false;
+        let on_value = Some(0);
         let off_value = Some(1);
 
-        let one_hot = one_hot(indices, depth, on_value, off_value)?;
+        let one_cold = one_hot::<i64, u8>(indices, depth, allow_f64, on_value, off_value)?;
 
-        let expected_matrix = [[[0, 1, 1, 1], [1, 1, 0, 1]], [[1, 0, 1, 1], [1, 1, 1, 0]]];
+        let expected_matrix = [[[0, 1, 1, 1], [1, 1, 0, 1]], [[1, 0, 1, 1], [1, 1, 1, 1]]];
 
-        assert_eq!(one_hot.shape(), &Shape::from((2, 2, 4)));
+        assert_eq!(one_cold.shape(), &Shape::from((2, 2, depth)));
 
-        let matrix = one_hot.to_vec3::<u8>()?;
+        let matrix = one_cold.to_vec3::<u8>()?;
 
         assert_eq!(matrix, expected_matrix);
 
