0a245e6fa46c16f332555a58271dbd49a8058a9c
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-12-29 20:00:12 -0300

    Metal: support unary abs (#1503)
    * Metal: support unary abs
    
    * cargo fmt

--- candle-metal-kernels/src/lib.rs
+++ candle-metal-kernels/src/lib.rs
@@ -173,7 +173,10 @@ macro_rules! ops{
 }
 
 pub mod unary {
-    ops!(cos, sin, exp, sqr, sqrt, neg, log, gelu, ceil, floor, round, erf, gelu_erf, tanh, recip);
+    ops!(
+        cos, sin, exp, sqr, sqrt, neg, log, gelu, abs, ceil, floor, round, erf, gelu_erf, tanh,
+        recip
+    );
 }
 pub mod binary {
     ops!(add, sub, mul, div, min, max, eq, ne, le, lt, ge, gt);
