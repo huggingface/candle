4a9d2039da92f897e0be63339787c8928622585d
Roma Klapaukh <rklapaukh@hotmail.com>
2024-02-06 13:17:43 +1100

    Add code to generate test pth data

---
+++ candle-core/tests/pth.py
@@ -0,0 +1,8 @@
+import torch
+from collections import OrderedDict
+
+# Write a trivial tensor to a pt file
+a= torch.tensor([[1,2,3,4], [5,6,7,8]])
+o = OrderedDict()
+o["test"] = a
+torch.save(o, "test.pt")
