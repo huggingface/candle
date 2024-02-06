20dd7248af4a684e6db5d622f15b98e0de6392ef
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2024-02-06 14:02:14 -0600

    Enhance pickle to retrieve state_dict with a given key

--- candle-core/tests/pth.py
+++ candle-core/tests/pth.py
@@ -6,3 +6,5 @@ a= torch.tensor([[1,2,3,4], [5,6,7,8]])
 o = OrderedDict()
 o["test"] = a
 torch.save(o, "test.pt")
+
+torch.save({"model_state_dict": o}, "test_with_key.pt")
--- candle-core/tests/pth_tests.rs
+++ candle-core/tests/pth_tests.rs
@@ -1,6 +1,14 @@
 /// Regression test for pth files not loading on Windows.
 #[test]
 fn test_pth() {
-    let tensors = candle_core::pickle::PthTensors::new("tests/test.pt").unwrap();
+    let tensors = candle_core::pickle::PthTensors::new("tests/test.pt", None).unwrap();
+    tensors.get("test").unwrap().unwrap();
+}
+
+#[test]
+fn test_pth_with_key() {
+    let tensors =
+        candle_core::pickle::PthTensors::new("tests/test_with_key.pt", Some("model_state_dict"))
+            .unwrap();
     tensors.get("test").unwrap().unwrap();
 }
