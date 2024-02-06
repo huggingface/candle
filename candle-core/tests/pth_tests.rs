adfae2460ac39a76fb15b1543ebe056c1d7b785f
Laurent Mazare <laurent.mazare@gmail.com>
2024-02-06 12:06:06 +0100

    Fix rustfmt. (#1669)

--- candle-core/tests/pth_tests.rs
+++ candle-core/tests/pth_tests.rs
@@ -1,6 +1,6 @@
 /// Regression test for pth files not loading on Windows.
 #[test]
-fn test_pth() {   
+fn test_pth() {
     let tensors = candle_core::pickle::PthTensors::new("tests/test.pt").unwrap();
     tensors.get("test").unwrap().unwrap();
 }
