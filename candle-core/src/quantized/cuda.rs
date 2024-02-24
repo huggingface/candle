c5ec1e2f99769326d2c5ccc2136d1eb52997a833
laurent <laurent.mazare@gmail.com>
2024-02-24 19:40:21 +0100

    More cuda quantization (quantize on cpu for now).

--- candle-core/src/quantized/cuda.rs
+++ candle-core/src/quantized/cuda.rs
@@ -1,15 +1,24 @@
-#![allow(unused)]
-use super::GgmlDType;
+use super::{GgmlDType, QStorage};
+use crate::cuda_backend::WrapErr;
 use crate::{CudaDevice, CudaStorage, Error, Result};
 
+use cudarc::driver::{CudaSlice, DeviceSlice};
+
 pub struct QCudaStorage {
+    data: CudaSlice<u8>,
     dtype: GgmlDType,
     device: CudaDevice,
 }
 
 impl QCudaStorage {
-    pub fn zeros(_: &CudaDevice, _: usize, _: GgmlDType) -> Result<Self> {
-        Err(Error::NotCompiledWithCudaSupport)
+    pub fn zeros(device: &CudaDevice, el_count: usize, dtype: GgmlDType) -> Result<Self> {
+        let size_in_bytes = el_count * dtype.type_size() / dtype.block_size();
+        let data = device.alloc_zeros::<u8>(size_in_bytes).w()?;
+        Ok(QCudaStorage {
+            data,
+            device: device.clone(),
+            dtype,
+        })
     }
 
     pub fn dtype(&self) -> GgmlDType {
@@ -21,15 +30,15 @@ impl QCudaStorage {
     }
 
     pub fn dequantize(&self, _elem_count: usize) -> Result<CudaStorage> {
-        Err(Error::NotCompiledWithCudaSupport)
+        Err(Error::NotCompiledWithCudaSupport.bt())
     }
 
     pub fn quantize(&mut self, _src: &CudaStorage) -> Result<()> {
-        Err(Error::NotCompiledWithCudaSupport)
+        Err(Error::NotCompiledWithCudaSupport.bt())
     }
 
     pub fn storage_size_in_bytes(&self) -> usize {
-        0
+        self.data.len()
     }
 
     pub fn fwd(
@@ -38,13 +47,20 @@ impl QCudaStorage {
         _storage: &CudaStorage,
         _layout: &crate::Layout,
     ) -> Result<(CudaStorage, crate::Shape)> {
-        Err(Error::NotCompiledWithCudaSupport)
+        Err(Error::NotCompiledWithCudaSupport.bt())
     }
 }
 
 pub fn load_quantized<T: super::GgmlType + Send + Sync + 'static>(
-    _device: &CudaDevice,
-    _data: &[T],
+    device: &CudaDevice,
+    data: &[T],
 ) -> Result<super::QStorage> {
-    Err(Error::NotCompiledWithCudaSupport)
+    let size_in_bytes = core::mem::size_of_val(data);
+    let data = unsafe { std::slice::from_raw_parts(data.as_ptr() as *const u8, size_in_bytes) };
+    let data = device.htod_sync_copy(data).w()?;
+    Ok(QStorage::Cuda(QCudaStorage {
+        data,
+        device: device.clone(),
+        dtype: T::DTYPE,
+    }))
 }
