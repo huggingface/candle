f924eee9a6e78800ba6737bd13d8f7e681af6c05
EricLBuehler <ericlbuehler@gmail.com>
2024-03-12 22:27:02 -0400

    Make bias either 0 or have value

--- candle-nn/src/layer_norm.rs
+++ candle-nn/src/layer_norm.rs
@@ -28,7 +28,7 @@
 //! ```
 //!
 //! [`Layer Normalization`]: https://arxiv.org/abs/1607.06450
-use std::{mem, sync::RwLockReadGuard};
+use std::mem;
 
 use candle::{
     backend::BackendStorage,
@@ -239,7 +239,7 @@ impl crate::Module for LayerNorm {
         let norm_x = (x.sqr()?.sum_keepdim(D::Minus1)? / hidden_size as f64)?;
         let x_normed = x.broadcast_div(&(norm_x + self.eps)?.sqrt()?)?;
         let x = x_normed.to_dtype(x_dtype)?.broadcast_mul(&self.weight)?;
-        x.broadcast_add(bias)
+        x.broadcast_add(&self.bias)
     }
 }
 
