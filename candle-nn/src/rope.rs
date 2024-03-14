4a1544c26aab1e756064451165647dd545821204
EricLBuehler <ericlbuehler@gmail.com>
2024-03-14 18:44:18 -0400

    Verify err

--- candle-nn/src/rope.rs
+++ candle-nn/src/rope.rs
@@ -72,8 +72,8 @@ impl RotaryEmbedding {
         )?;
 
         let cfg = LaunchConfig {
-            grid_dim: (512.min((num_heads * rot_dim / 2) as u32), 1, 1),//(num_tokens as u32, 1, 1),
-            block_dim: (num_tokens as u32, 1, 1),//(512.min((num_heads * rot_dim / 2) as u32), 1, 1),
+            grid_dim: (num_tokens as u32, 1, 1),
+            block_dim: (512.min((num_heads * rot_dim / 2) as u32), 1, 1),
             shared_mem_bytes: 0,
         };
 
@@ -163,6 +163,8 @@ impl RotaryEmbedding {
         k: &mut Tensor,
         is_neox: bool,
     ) -> Result<()> {
+        *q = q.contiguous()?;
+        *k = k.contiguous()?;
         match (q.device(), k.device()) {
             #[cfg(feature = "cuda")]
             (Device::Cuda(dev), Device::Cuda(_)) => {
