e3df9ef0d85201a18aa75d296896fd9508d22ca5
EricLBuehler <ericlbuehler@gmail.com>
2024-03-15 19:08:49 -0400

    Make s_id for cache

--- candle-kernels/src/fused_rope.cu
+++ candle-kernels/src/fused_rope.cu
@@ -240,13 +240,14 @@ __device__ void fused_rope_cached_forward(
     const int o_stride_d, const scalar_t_0* src, const scalar_t_1* cos,
     const scalar_t_1* sin, scalar_t_0* dst, int64_t* positions) {
   int b_id = blockIdx.y;
-  int s_id = blockIdx.x + positions[b_id];
+  int s_id = blockIdx.x;
+  int s_id_cache = blockIdx.x + positions[b_id];
   int offset_block = s_id * stride_s + b_id * stride_b;
   int offset_block_dst = s_id * o_stride_s + b_id * o_stride_b;
 #pragma unroll
   for (int d_id = threadIdx.x; d_id < d2; d_id += blockDim.x) {
-    scalar_t_0 v_cos = cos[s_id * d2 + d_id];
-    scalar_t_0 v_sin = sin[s_id * d2 + d_id];
+    scalar_t_0 v_cos = cos[s_id_cache * d2 + d_id];
+    scalar_t_0 v_sin = sin[s_id_cache * d2 + d_id];
 #pragma unroll
     for (int h_id = threadIdx.y; h_id < h; h_id += blockDim.y) {
       int offset_src = offset_block + h_id * stride_h + d_id * stride_d;
