75c1f638b80a0a8e8d468d16224702975dd795bf
EricLBuehler <ericlbuehler@gmail.com>
2024-03-15 05:22:38 -0400

    Conv to tensor

--- candle-nn/src/rope.rs
+++ candle-nn/src/rope.rs
@@ -89,13 +89,13 @@ impl RotaryEmbedding {
         dbg!(self.cache.shape());
 
         let positions = positions.iter().map(|x| *x as i64).collect::<Vec<_>>();
-        let positions = Tensor::new(positions.as_slice(), device)?;
+        let positions = Tensor::new(positions.as_slice(), q.device())?;
         let pos_storage = match  &*positions.storage_and_layout().0{
             Storage::Cuda(storage) => {
                 storage
             }
             _ => {
-                unreachable!(;)
+                unreachable!();
             }
         };
         let params = (
