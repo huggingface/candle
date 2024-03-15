2bbf24f0a3531280b5dbc4a9a44548977bc28e1d
EricLBuehler <ericlbuehler@gmail.com>
2024-03-15 19:44:56 -0400

    Check mean cmp

--- candle-nn/src/rope.rs
+++ candle-nn/src/rope.rs
@@ -281,6 +281,8 @@ impl RotaryEmbedding {
                 // want (seqlen, bs, num_head, head_dim)
                 let in_q = q.permute((1, 0, 2, 3))?;
                 let in_k = k.permute((1, 0, 2, 3))?;
+                dbg!(in_q.mean_all()?);
+                dbg!(q.mean_all()?);
                 let (new_q, new_k) = self.fused_rope(dev, positions, &in_q, &in_k, is_neox)?;
                 // output is (seqlen, bs, num_head, head_dim)
                 // want (bs, seqlen, num_head, head_dim)
