67b62e0f51e1624efe3003755bf584ba1b00f4cb
EricLBuehler <ericlbuehler@gmail.com>
2024-03-15 19:27:59 -0400

    Offset positions in idx

--- candle-kernels/src/fused_rope.cu
+++ candle-kernels/src/fused_rope.cu
@@ -244,9 +244,9 @@ __device__ void fused_rope_cached_forward(
   int offset_block = s_id * stride_s + b_id * stride_b;
   int offset_block_dst = s_id * o_stride_s + b_id * o_stride_b;
 #pragma unroll
-  for (int d_id = threadIdx.x; d_id < (d2 - positions[b_id]); d_id += blockDim.x) {
-    scalar_t_0 v_cos = cos[s_id * d2 + d_id+ positions[b_id]];
-    scalar_t_0 v_sin = sin[s_id * d2 + d_id+ positions[b_id]];
+  for (int d_id = threadIdx.x; d_id < d2; d_id += blockDim.x) {
+    scalar_t_0 v_cos = cos[s_id * d2 + d_id];
+    scalar_t_0 v_sin = sin[s_id * d2 + d_id];
 #pragma unroll
     for (int h_id = threadIdx.y; h_id < h; h_id += blockDim.y) {
       int offset_src = offset_block + h_id * stride_h + d_id * stride_d;
