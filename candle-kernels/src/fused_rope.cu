928acb613ba803f909eb8bc7639d933637172553
EricLBuehler <ericlbuehler@gmail.com>
2024-03-16 17:46:37 -0400

    Freeze

--- candle-kernels/src/fused_rope.cu
+++ candle-kernels/src/fused_rope.cu
@@ -11,7 +11,7 @@ __device__ void fused_rope_cached_forward(
   int s_id = blockIdx.x;
   int offset_block = s_id * stride_s + b_id * stride_b;
   int offset_block_dst = s_id * stride_s + b_id * stride_b;
-  int pos = positions[b_id*pos_block_stride + s_id];
+  int pos = positions[b_id * pos_block_stride + s_id];
 #pragma unroll
   for (int d_id = threadIdx.x; d_id < d2; d_id += blockDim.x) {
     scalar_t_0 v_cos = cos[(pos + s_id) * d2 + d_id];
