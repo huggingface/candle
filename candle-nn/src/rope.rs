d8bbd3052143118ff34787564b0b7860775ee524
EricLBuehler <ericlbuehler@gmail.com>
2024-03-16 12:53:57 -0400

    Remove prints and unnecessary contig

--- candle-nn/src/rope.rs
+++ candle-nn/src/rope.rs
@@ -329,9 +329,6 @@ impl RotaryEmbedding {
         k: &mut Tensor,
         is_neox: bool,
     ) -> Result<()> {
-        dbg!(q.mean_all()?);
-        *q = q.contiguous()?;
-        *k = k.contiguous()?;
         match (q.device(), k.device()) {
             #[cfg(feature = "cuda")]
             (Device::Cuda(dev), Device::Cuda(_)) => {
@@ -339,8 +336,6 @@ impl RotaryEmbedding {
                 // want (seqlen, bs, num_head, head_dim)
                 let in_q = q.permute((1, 0, 2, 3))?;
                 let in_k = k.permute((1, 0, 2, 3))?;
-                dbg!(in_q.mean_all()?);
-                dbg!(q.shape());
                 let (new_q, new_k) = self.fused_rope(dev, positions, &in_q, &in_k, is_neox)?;
                 // output is (seqlen, bs, num_head, head_dim)
                 // want (bs, seqlen, num_head, head_dim)
@@ -355,8 +350,6 @@ impl RotaryEmbedding {
                 *k = self.apply_rotary_emb(&*k, positions)?;
             }
         };
-        *q = q.contiguous()?;
-        *k = k.contiguous()?;
         Ok(())
     }
 
