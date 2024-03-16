4c30544826f07c9c8af40a8b0f22e90b7e087e23
EricLBuehler <ericlbuehler@gmail.com>
2024-03-16 17:35:00 -0400

    Add stride arg

--- candle-kernels/src/fused_rope.cu
+++ candle-kernels/src/fused_rope.cu
@@ -11,7 +11,7 @@ __device__ void fused_rope_cached_forward(
   int s_id = blockIdx.x;
   int offset_block = s_id * stride_s + b_id * stride_b;
   int offset_block_dst = s_id * stride_s + b_id * stride_b;
-  int pos = positions[b_id*pos_block_stride + s_id]; //s_id
+  int pos = positions[s_id*pos_block_stride + b_id]; //s_id
 #pragma unroll
   for (int d_id = threadIdx.x; d_id < d2; d_id += blockDim.x) {
     scalar_t_0 v_cos = cos[pos * d2 + d_id];
