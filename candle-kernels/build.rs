13ae5a34c76af5c8226da09819d31c4cd70c49c9
Laurent Mazare <laurent.mazare@gmail.com>
2024-03-28 06:56:48 +0100

    Ensure that the kernels get rebuilt on cuh changes. (#1954)

--- candle-kernels/build.rs
+++ candle-kernels/build.rs
@@ -1,5 +1,8 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=src/compatibility.cuh");
+    println!("cargo:rerun-if-changed=src/cuda_utils.cuh");
+    println!("cargo:rerun-if-changed=src/binary_op_macros.cuh");
 
     let builder = bindgen_cuda::Builder::default();
     println!("cargo:info={builder:?}");
