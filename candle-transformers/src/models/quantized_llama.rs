677f22d8401084052bc1fbd9c8dd1489098497f5
laurent <laurent.mazare@gmail.com>
2024-04-11 21:29:00 +0200

    Move the function to utils + use it in mistral.

--- candle-transformers/src/models/quantized_llama.rs
+++ candle-transformers/src/models/quantized_llama.rs
@@ -206,8 +206,8 @@ impl LayerWeights {
         self.kv_cache = Some((k.clone(), v.clone()));
 
         // Support for MQA, useful for 70B models and mistral.
-        let k = self.repeat_kv(k)?;
-        let v = self.repeat_kv(v)?;
+        let k = crate::utils::repeat_kv(k, self.n_head / self.n_kv_head)?;
+        let v = crate::utils::repeat_kv(v, self.n_head / self.n_kv_head)?;
 
         let att = (q.matmul(&k.t()?)? / (self.head_dim as f64).sqrt())?;
         let att = match mask {
@@ -223,16 +223,6 @@ impl LayerWeights {
         let y = y.transpose(1, 2)?.reshape(&[b_sz, seq_len, n_embd])?;
         let y = self.attention_wo.forward(&y)?;
         Ok(y)
-    }
-
-    fn repeat_kv(&self, x: Tensor) -> Result<Tensor> {
-        let n_rep = self.n_head / self.n_kv_head;
-        if n_rep == 1 {
-            Ok(x)
-        } else {
-            let (b_sz, n_kv_head, seq_len, head_dim) = x.dims4()?;
-            Tensor::cat(&vec![&x; n_rep], 2)?.reshape((b_sz, n_kv_head * n_rep, seq_len, head_dim))
-        }
     }
 }
 
