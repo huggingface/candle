d0f9c720ac896b4f7186071e29bcc4eab90023de
Thomas Santerre <thomas@santerre.xyz>
2024-04-20 11:03:34 -0400

    undo bench ignore for other tests

--- candle-core/benches/bench_main.rs
+++ candle-core/benches/bench_main.rs
@@ -2,11 +2,11 @@ mod benchmarks;
 
 use criterion::criterion_main;
 criterion_main!(
-    // benchmarks::affine::benches,
-    // benchmarks::matmul::benches,
-    // benchmarks::random::benches,
-    // benchmarks::where_cond::benches,
-    // benchmarks::conv_transpose2d::benches,
-    // benchmarks::qmatmul::benches,
+    benchmarks::affine::benches,
+    benchmarks::matmul::benches,
+    benchmarks::random::benches,
+    benchmarks::where_cond::benches,
+    benchmarks::conv_transpose2d::benches,
+    benchmarks::qmatmul::benches,
     benchmarks::unary::benches
 );
