5459b93376cdf3a111c3a246fc613d06397eee01
Laurent <laurent.mazare@gmail.com>
2024-04-27 12:52:24 +0200

    Hook the cuda kernel + rework the cpu bits.

--- candle-core/src/sort.rs
+++ candle-core/src/sort.rs
@@ -2,32 +2,46 @@ use crate::{Result, Tensor};
 use rayon::prelude::*;
 
 #[derive(Debug, Clone, Copy)]
-struct ArgSort;
+struct ArgSort {
+    asc: bool,
+    last_dim: usize,
+}
 
-fn asort<T: crate::WithDType>(vs: &[T], layout: &crate::Layout) -> Result<Vec<u32>> {
-    #[allow(clippy::uninit_vec)]
-    // Safety: indexes are set later in the parallelized section.
-    let mut sort_indexes = unsafe {
-        let el_count = layout.shape().elem_count();
-        let mut v = Vec::with_capacity(el_count);
-        v.set_len(el_count);
-        v
-    };
-    let last_dim = match layout.dims().last() {
-        None => crate::bail!("empty last-dim in arg-sort"),
-        Some(last_dim) => *last_dim,
-    };
-    sort_indexes
-        .par_chunks_exact_mut(last_dim)
-        .zip(vs.par_chunks_exact(last_dim))
-        .for_each(|(indexes, vs)| {
-            indexes.sort_by(|&i, &j| {
-                vs[i as usize]
-                    .partial_cmp(&vs[j as usize])
-                    .unwrap_or(std::cmp::Ordering::Greater)
-            })
-        });
-    Ok(sort_indexes)
+impl ArgSort {
+    fn asort<T: crate::WithDType>(&self, vs: &[T], layout: &crate::Layout) -> Vec<u32> {
+        #[allow(clippy::uninit_vec)]
+        // Safety: indexes are set later in the parallelized section.
+        let mut sort_indexes = unsafe {
+            let el_count = layout.shape().elem_count();
+            let mut v = Vec::with_capacity(el_count);
+            v.set_len(el_count);
+            v
+        };
+        if self.asc {
+            sort_indexes
+                .par_chunks_exact_mut(self.last_dim)
+                .zip(vs.par_chunks_exact(self.last_dim))
+                .for_each(|(indexes, vs)| {
+                    indexes.sort_by(|&i, &j| {
+                        vs[i as usize]
+                            .partial_cmp(&vs[j as usize])
+                            .unwrap_or(std::cmp::Ordering::Greater)
+                    })
+                });
+        } else {
+            sort_indexes
+                .par_chunks_exact_mut(self.last_dim)
+                .zip(vs.par_chunks_exact(self.last_dim))
+                .for_each(|(indexes, vs)| {
+                    indexes.sort_by(|&j, &i| {
+                        vs[i as usize]
+                            .partial_cmp(&vs[j as usize])
+                            .unwrap_or(std::cmp::Ordering::Greater)
+                    })
+                });
+        }
+        sort_indexes
+    }
 }
 
 impl crate::CustomOp1 for ArgSort {
@@ -41,14 +55,14 @@ impl crate::CustomOp1 for ArgSort {
         layout: &crate::Layout,
     ) -> Result<(crate::CpuStorage, crate::Shape)> {
         let sort_indexes = match storage {
-            crate::CpuStorage::U8(vs) => asort(vs, layout),
-            crate::CpuStorage::U32(vs) => asort(vs, layout),
-            crate::CpuStorage::I64(vs) => asort(vs, layout),
-            crate::CpuStorage::BF16(vs) => asort(vs, layout),
-            crate::CpuStorage::F16(vs) => asort(vs, layout),
-            crate::CpuStorage::F32(vs) => asort(vs, layout),
-            crate::CpuStorage::F64(vs) => asort(vs, layout),
-        }?;
+            crate::CpuStorage::U8(vs) => self.asort(vs, layout),
+            crate::CpuStorage::U32(vs) => self.asort(vs, layout),
+            crate::CpuStorage::I64(vs) => self.asort(vs, layout),
+            crate::CpuStorage::BF16(vs) => self.asort(vs, layout),
+            crate::CpuStorage::F16(vs) => self.asort(vs, layout),
+            crate::CpuStorage::F32(vs) => self.asort(vs, layout),
+            crate::CpuStorage::F64(vs) => self.asort(vs, layout),
+        };
         let sort_indexes = crate::CpuStorage::U32(sort_indexes);
         Ok((sort_indexes, layout.shape().into()))
     }
@@ -56,10 +70,39 @@ impl crate::CustomOp1 for ArgSort {
     #[cfg(feature = "cuda")]
     fn cuda_fwd(
         &self,
-        _storage: &crate::CudaStorage,
-        _layout: &crate::Layout,
+        storage: &crate::CudaStorage,
+        layout: &crate::Layout,
     ) -> Result<(crate::CudaStorage, crate::Shape)> {
-        todo!()
+        use crate::backend::BackendStorage;
+        use crate::cuda_backend::cudarc::driver::{LaunchAsync, LaunchConfig};
+        use crate::cuda_backend::WrapErr;
+        let dev = storage.device().clone();
+        // TODO: handle other dtypes
+        let slice = storage.as_cuda_slice::<f32>()?;
+        let slice = match layout.contiguous_offsets() {
+            None => crate::bail!("input has to be contiguous"),
+            Some((o1, o2)) => slice.slice(o1..o2),
+        };
+        let elem_count = layout.shape().elem_count();
+        let dst = unsafe { dev.alloc::<u32>(elem_count) }.w()?;
+        let func = if self.asc {
+            dev.get_or_load_func("asort_asc_f32", candle_kernels::SORT)?
+        } else {
+            dev.get_or_load_func("asort_desc_f32", candle_kernels::SORT)?
+        };
+        let ncols = self.last_dim;
+        let nrows = elem_count / ncols;
+        let ncols_pad = next_power_of_2(ncols);
+        let params = (&slice, &dst, ncols as i32, ncols_pad as i32);
+        let cfg = LaunchConfig {
+            grid_dim: (1, nrows as u32, 1),
+            block_dim: (ncols_pad as u32, 1, 1),
+            shared_mem_bytes: (ncols_pad * std::mem::size_of::<u32>()) as u32,
+        };
+        unsafe { func.launch(cfg, params) }.w()?;
+
+        let dst = crate::CudaStorage::wrap_cuda_slice(dst, dev);
+        Ok((dst, layout.shape().clone()))
     }
 
     #[cfg(feature = "metal")]
@@ -72,14 +115,31 @@ impl crate::CustomOp1 for ArgSort {
     }
 }
 
+#[allow(unused)]
+fn next_power_of_2(x: usize) -> usize {
+    let mut n = 1;
+    while n < x {
+        n *= 2
+    }
+    n
+}
+
 impl Tensor {
-    pub fn arg_sort_last_dim(&self) -> Result<Tensor> {
+    /// Returns the indices that sort the tensor along the last dimension.
+    ///
+    /// If `asc` is `true`, sorting is in ascending order. Otherwise sorting is performed in
+    /// descending order.
+    pub fn arg_sort_last_dim(&self, asc: bool) -> Result<Tensor> {
         if !self.is_contiguous() {
             return Err(crate::Error::RequiresContiguous {
                 op: "arg_sort_last_dim",
             });
         }
+        let last_dim = match self.dims().last() {
+            None => crate::bail!("empty last-dim in arg-sort"),
+            Some(last_dim) => *last_dim,
+        };
         // No need for a backward pass for arg sort.
-        self.apply_op1_no_bwd(&ArgSort)
+        self.apply_op1_no_bwd(&ArgSort { asc, last_dim })
     }
 }
