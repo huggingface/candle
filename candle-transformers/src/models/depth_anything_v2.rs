a3012fe5e58b924a9a7a3428507fb16331f3dffb
Jeroen Vlek <jeroen@perceptivebits.com>
2024-06-17 18:08:37 +0200

    construct ResidualConvUnit

--- candle-transformers/src/models/depth_anything_v2.rs
+++ candle-transformers/src/models/depth_anything_v2.rs
@@ -1,4 +1,5 @@
-use candle_nn::{Activation, BatchNorm, Conv2d, Sequential};
+use candle::Result;
+use candle_nn::{Activation, batch_norm, BatchNorm, BatchNormConfig, Conv2d, conv2d, Conv2dConfig, Sequential, VarBuilder};
 
 use crate::models::dinov2::DinoVisionTransformer;
 
@@ -8,6 +9,43 @@ pub struct ResidualConvUnit {
     conv2: Conv2d,
     batch_norm1: Option<BatchNorm>,
     batch_norm2: Option<BatchNorm>,
+}
+
+impl ResidualConvUnit {
+    pub fn new(num_features: usize, activation: Activation, use_batch_norm: bool, var_builder: VarBuilder) -> Result<Self> {
+        const KERNEL_SIZE: usize = 3;
+        let conv_cfg = Conv2dConfig {
+            padding: 1,
+            stride: 1,
+            dilation: 1,
+            groups: 1,
+        };
+        let conv1 = conv2d(num_features, num_features, KERNEL_SIZE, conv_cfg, var_builder.push_prefix("conv1"))?;
+        let conv2 = conv2d(num_features, num_features, KERNEL_SIZE, conv_cfg, var_builder.push_prefix("conv2"))?;
+
+        
+        let (batch_norm1, batch_norm2) = match use_batch_norm {
+            true => {
+                let batch_norm_cfg = BatchNormConfig {
+                    eps: 1e-05,
+                    remove_mean: false,
+                    affine: true,
+                    momentum: 0.1,
+                };
+                (Some(batch_norm(num_features, batch_norm_cfg, var_builder.push_prefix("batch_norm1"))?), Some(batch_norm(num_features, batch_norm_cfg, var_builder.push_prefix("batch_norm2"))?))
+            },
+            false => (None, None)
+        };
+
+        Ok(Self {
+          activation,
+            conv1,
+            conv2,
+            batch_norm1,
+            batch_norm2
+        })
+
+    }
 }
 
 pub struct FeatureFusionBlock {
