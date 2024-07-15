047be5b61ad37f6e7fad8e156a935e05a9f7a6c2
laurent <laurent.mazare@gmail.com>
2024-07-15 19:36:42 +0200

    Update the flash attn kernels.

--- candle-flash-attn/kernels/flash_fwd_hdim160_bf16_causal_sm80.cu
+++ candle-flash-attn/kernels/flash_fwd_hdim160_bf16_causal_sm80.cu
@@ -5,6 +5,6 @@
 #include "flash_fwd_launch_template.h"
 
 template<>
-void run_mha_fwd_<cutlass::half_t, 32, false>(Flash_fwd_params &params, cudaStream_t stream) {
-    run_mha_fwd_hdim32<cutlass::half_t, false>(params, stream);
+void run_mha_fwd_<cutlass::bfloat16_t, 160, true>(Flash_fwd_params &params, cudaStream_t stream) {
+    run_mha_fwd_hdim160<cutlass::bfloat16_t, true>(params, stream);
 }
