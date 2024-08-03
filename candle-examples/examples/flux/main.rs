9c2befb5bb5225f8a401b4c76b7b7031b4785c80
laurent <laurent.mazare@gmail.com>
2024-08-03 21:30:06 +0200

    Add the decode only mode.

--- candle-examples/examples/flux/main.rs
+++ candle-examples/examples/flux/main.rs
@@ -34,6 +34,9 @@ struct Args {
     /// The width in pixels of the generated image.
     #[arg(long)]
     width: Option<usize>,
+
+    #[arg(long)]
+    decode_only: Option<String>,
 }
 
 fn run(args: Args) -> Result<()> {
@@ -46,6 +49,7 @@ fn run(args: Args) -> Result<()> {
         height,
         width,
         tracing,
+        decode_only,
     } = args;
     let width = width.unwrap_or(1360);
     let height = height.unwrap_or(768);
@@ -59,95 +63,107 @@ fn run(args: Args) -> Result<()> {
     };
 
     let api = hf_hub::api::sync::Api::new()?;
-    let device = candle_examples::device(cpu)?;
-    let dtype = device.bf16_default_to_f32();
-    let t5_emb = {
-        let repo = api.repo(hf_hub::Repo::with_revision(
-            "google/t5-v1_1-xxl".to_string(),
-            hf_hub::RepoType::Model,
-            "refs/pr/2".to_string(),
-        ));
-        let model_file = repo.get("model.safetensors")?;
-        let vb = unsafe { VarBuilder::from_mmaped_safetensors(&[model_file], dtype, &device)? };
-        let config_filename = repo.get("config.json")?;
-        let config = std::fs::read_to_string(config_filename)?;
-        let config: t5::Config = serde_json::from_str(&config)?;
-        let mut model = t5::T5EncoderModel::load(vb, &config)?;
-        let tokenizer_filename = api
-            .model("lmz/mt5-tokenizers".to_string())
-            .get("t5-v1_1-xxl.tokenizer.json")?;
-        let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
-        let mut tokens = tokenizer
-            .encode(prompt.as_str(), true)
-            .map_err(E::msg)?
-            .get_ids()
-            .to_vec();
-        tokens.resize(256, 0);
-        let input_token_ids = Tensor::new(&tokens[..], &device)?.unsqueeze(0)?;
-        println!("{input_token_ids}");
-        model.forward(&input_token_ids)?
-    };
-    println!("T5\n{t5_emb}");
-    let clip_emb = {
-        let repo = api.repo(hf_hub::Repo::model(
-            "openai/clip-vit-large-patch14".to_string(),
-        ));
-        let model_file = repo.get("model.safetensors")?;
-        let vb = unsafe { VarBuilder::from_mmaped_safetensors(&[model_file], dtype, &device)? };
-        // https://huggingface.co/openai/clip-vit-large-patch14/blob/main/config.json
-        let config = clip::text_model::ClipTextConfig {
-            vocab_size: 49408,
-            projection_dim: 768,
-            activation: clip::text_model::Activation::QuickGelu,
-            intermediate_size: 3072,
-            embed_dim: 768,
-            max_position_embeddings: 77,
-            pad_with: None,
-            num_hidden_layers: 12,
-            num_attention_heads: 12,
-        };
-        let model = clip::text_model::ClipTextTransformer::new(vb.pp("text_model"), &config)?;
-        let tokenizer_filename = repo.get("tokenizer.json")?;
-        let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
-        let tokens = tokenizer
-            .encode(prompt.as_str(), true)
-            .map_err(E::msg)?
-            .get_ids()
-            .to_vec();
-        let input_token_ids = Tensor::new(&tokens[..], &device)?.unsqueeze(0)?;
-        println!("{input_token_ids}");
-        model.forward(&input_token_ids)?
-    };
-    println!("CLIP\n{clip_emb}");
-    let repo = api.repo(hf_hub::Repo::model(
+    let bf_repo = api.repo(hf_hub::Repo::model(
         "black-forest-labs/FLUX.1-schnell".to_string(),
     ));
-    let img = {
-        let model_file = repo.get("flux1-schnell.sft")?;
-        let vb = unsafe { VarBuilder::from_mmaped_safetensors(&[model_file], dtype, &device)? };
-        let cfg = flux::model::Config::schnell();
-        let model = flux::model::Flux::new(&cfg, vb)?;
+    let device = candle_examples::device(cpu)?;
+    let dtype = device.bf16_default_to_f32();
+    let img = match decode_only {
+        None => {
+            let t5_emb = {
+                let repo = api.repo(hf_hub::Repo::with_revision(
+                    "google/t5-v1_1-xxl".to_string(),
+                    hf_hub::RepoType::Model,
+                    "refs/pr/2".to_string(),
+                ));
+                let model_file = repo.get("model.safetensors")?;
+                let vb =
+                    unsafe { VarBuilder::from_mmaped_safetensors(&[model_file], dtype, &device)? };
+                let config_filename = repo.get("config.json")?;
+                let config = std::fs::read_to_string(config_filename)?;
+                let config: t5::Config = serde_json::from_str(&config)?;
+                let mut model = t5::T5EncoderModel::load(vb, &config)?;
+                let tokenizer_filename = api
+                    .model("lmz/mt5-tokenizers".to_string())
+                    .get("t5-v1_1-xxl.tokenizer.json")?;
+                let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
+                let mut tokens = tokenizer
+                    .encode(prompt.as_str(), true)
+                    .map_err(E::msg)?
+                    .get_ids()
+                    .to_vec();
+                tokens.resize(256, 0);
+                let input_token_ids = Tensor::new(&tokens[..], &device)?.unsqueeze(0)?;
+                println!("{input_token_ids}");
+                model.forward(&input_token_ids)?
+            };
+            println!("T5\n{t5_emb}");
+            let clip_emb = {
+                let repo = api.repo(hf_hub::Repo::model(
+                    "openai/clip-vit-large-patch14".to_string(),
+                ));
+                let model_file = repo.get("model.safetensors")?;
+                let vb =
+                    unsafe { VarBuilder::from_mmaped_safetensors(&[model_file], dtype, &device)? };
+                // https://huggingface.co/openai/clip-vit-large-patch14/blob/main/config.json
+                let config = clip::text_model::ClipTextConfig {
+                    vocab_size: 49408,
+                    projection_dim: 768,
+                    activation: clip::text_model::Activation::QuickGelu,
+                    intermediate_size: 3072,
+                    embed_dim: 768,
+                    max_position_embeddings: 77,
+                    pad_with: None,
+                    num_hidden_layers: 12,
+                    num_attention_heads: 12,
+                };
+                let model =
+                    clip::text_model::ClipTextTransformer::new(vb.pp("text_model"), &config)?;
+                let tokenizer_filename = repo.get("tokenizer.json")?;
+                let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
+                let tokens = tokenizer
+                    .encode(prompt.as_str(), true)
+                    .map_err(E::msg)?
+                    .get_ids()
+                    .to_vec();
+                let input_token_ids = Tensor::new(&tokens[..], &device)?.unsqueeze(0)?;
+                println!("{input_token_ids}");
+                model.forward(&input_token_ids)?
+            };
+            println!("CLIP\n{clip_emb}");
+            let img = {
+                let model_file = bf_repo.get("flux1-schnell.sft")?;
+                let vb =
+                    unsafe { VarBuilder::from_mmaped_safetensors(&[model_file], dtype, &device)? };
+                let cfg = flux::model::Config::schnell();
+                let model = flux::model::Flux::new(&cfg, vb)?;
 
-        let img = flux::sampling::get_noise(1, height, width, &device)?.to_dtype(dtype)?;
-        let state = flux::sampling::State::new(&t5_emb, &clip_emb, &img)?;
-        println!("{state:?}");
-        let timesteps = flux::sampling::get_schedule(4, None); // no shift for flux-schnell
-        flux::sampling::denoise(
-            &model,
-            &state.img,
-            &state.img_ids,
-            &state.txt,
-            &state.txt_ids,
-            &state.vec,
-            &timesteps,
-            4.,
-        )?
+                let img = flux::sampling::get_noise(1, height, width, &device)?.to_dtype(dtype)?;
+                let state = flux::sampling::State::new(&t5_emb, &clip_emb, &img)?;
+                println!("{state:?}");
+                let timesteps = flux::sampling::get_schedule(4, None); // no shift for flux-schnell
+                flux::sampling::denoise(
+                    &model,
+                    &state.img,
+                    &state.img_ids,
+                    &state.txt,
+                    &state.txt_ids,
+                    &state.vec,
+                    &timesteps,
+                    4.,
+                )?
+            };
+            flux::sampling::unpack(&img, height, width)?
+        }
+        Some(file) => {
+            let mut st = candle::safetensors::load(file, &device)?;
+            st.remove("img").unwrap()
+        }
     };
     println!("latent img\n{img}");
-    let img = flux::sampling::unpack(&img, height, width)?;
 
     let img = {
-        let model_file = repo.get("ae.sft")?;
+        let model_file = bf_repo.get("ae.sft")?;
         let vb = unsafe { VarBuilder::from_mmaped_safetensors(&[model_file], dtype, &device)? };
         let cfg = flux::autoencoder::Config::schnell();
         let model = flux::autoencoder::AutoEncoder::new(&cfg, vb)?;
