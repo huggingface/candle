c830f2632b1aba9fb950ee5c378281f48cfd3d1c
Eric Buehler <ericlbuehler@gmail.com>
2024-08-04 19:34:36 -0400

    Use matmul_with_alpha in sdpa

--- candle-nn/src/attention.rs
+++ candle-nn/src/attention.rs
@@ -28,11 +28,9 @@ pub fn scaled_dot_product_attention(
     q: &Tensor,
     k: &Tensor,
     v: &Tensor,
-    n_attn_heads: usize,
-    head_dim: usize,
+    scale: f64,
     mask: Option<Tensor>,
     use_flash_attn: bool,
-    b_sz: usize,
     seq_len: usize,
 ) -> Result<Tensor> {
     if use_flash_attn {
@@ -40,20 +38,18 @@ pub fn scaled_dot_product_attention(
         let q = q.transpose(1, 2)?;
         let k = k.transpose(1, 2)?;
         let v = v.transpose(1, 2)?;
-        let softmax_scale = 1f32 / (head_dim as f32).sqrt();
-        return flash_attn(&q, &k, &v, softmax_scale, seq_len > 1)?.transpose(1, 2);
+        return flash_attn(&q, &k, &v, scale as f32, seq_len > 1)?.transpose(1, 2);
     }
 
     let att = match mask {
         Some(mut m) => {
-            q.contiguous()?.matmul_with_alpha_beta(
-                &k.t()?.contiguous()?,
-                &mut m,
-                Some(1. / (head_dim as f64).sqrt()),
-            )?;
+            q.contiguous()?
+                .matmul_with_alpha_beta(&k.t()?.contiguous()?, &mut m, Some(scale))?;
             m
         }
-        None => (&q.contiguous()?.matmul(&k.t()?.contiguous()?)? / (head_dim as f64).sqrt())?,
+        None => q
+            .contiguous()?
+            .matmul_with_alpha(&k.t()?.contiguous()?, Some(scale))?,
     };
 
     let att = crate::ops::softmax_last_dim(&att)?;
