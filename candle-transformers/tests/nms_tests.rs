2be534faa77edd538422c41d842b61f84a094f2e
Matthew O <mathonich@yahoo.com>
2024-08-05 15:15:44 -0700

    NMS Test

---
+++ candle-transformers/tests/nms_tests.rs
@@ -0,0 +1,23 @@
+use candle::{Result};
+use candle_transformers::object_detection::{Bbox, non_maximum_suppression, soft_non_maximum_suppression};
+
+
+ #[test]
+fn test_non_maximum_suppression() -> Result<()> {
+    // Boxes based upon https://thepythoncode.com/article/non-maximum-suppression-using-opencv-in-python
+    let mut bboxes= vec![
+        vec![
+            Bbox { xmin: 245.0, ymin: 305.0, xmax: 575.0, ymax: 490.0, confidence: 0.9, data: () }, // Box 1
+            Bbox { xmin: 235.0, ymin: 300.0, xmax: 485.0, ymax: 515.0, confidence: 0.8, data: () }, // Box 2
+            Bbox { xmin: 305.0, ymin: 270.0, xmax: 540.0, ymax: 500.0, confidence: 0.6, data: () }  // Box 3
+        ]
+    ];
+
+    non_maximum_suppression(&mut bboxes, 0.5);
+    let bboxes = bboxes.into_iter().next().unwrap();
+    assert_eq!(bboxes.len(), 1);
+    assert_eq!(bboxes[0].confidence, 0.9);
+
+    Ok(())
+}
+
