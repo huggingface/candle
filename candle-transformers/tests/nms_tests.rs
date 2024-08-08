d4ffc198fc51d83e3741967e1d923f7145f39004
Matthew O <mathonich@yahoo.com>
2024-08-08 15:58:17 -0700

    Softnms basic functionality test

--- candle-transformers/tests/nms_tests.rs
+++ candle-transformers/tests/nms_tests.rs
@@ -3,7 +3,7 @@ use candle_transformers::object_detection::{Bbox, non_maximum_suppression, soft_
 
 
  #[test]
-fn test_non_maximum_suppression() -> Result<()> {
+fn nms_basic() -> Result<()> {
     // Boxes based upon https://thepythoncode.com/article/non-maximum-suppression-using-opencv-in-python
     let mut bboxes= vec![
         vec![
@@ -22,20 +22,16 @@ fn test_non_maximum_suppression() -> Result<()> {
 }
 
 #[test]
-fn test_soft_non_maximum_suppression() -> Result<()> {
-    // Boxes based upon https://thepythoncode.com/article/non-maximum-suppression-using-opencv-in-python
-    let mut bboxes= vec![
+fn softnms_basic_functionality() {
+    let mut bboxes = vec![
         vec![
-            Bbox { xmin: 245.0, ymin: 305.0, xmax: 575.0, ymax: 490.0, confidence: 0.9, data: () }, // Box 1
-            Bbox { xmin: 235.0, ymin: 300.0, xmax: 485.0, ymax: 515.0, confidence: 0.8, data: () }, // Box 2
-            Bbox { xmin: 305.0, ymin: 270.0, xmax: 540.0, ymax: 500.0, confidence: 0.6, data: () }  // Box 3
-        ]
+            Bbox { xmin: 0.0, ymin: 0.0, xmax: 1.0, ymax: 1.0, confidence: 0.9, data: () },
+            Bbox { xmin: 0.1, ymin: 0.1, xmax: 1.1, ymax: 1.1, confidence: 0.8, data: () },
+            Bbox { xmin: 0.2, ymin: 0.2, xmax: 1.2, ymax: 1.2, confidence: 0.7, data: () },
+        ],
     ];
 
-    soft_non_maximum_suppression(&mut bboxes, Some(0.5), Some(0.3), Some(0.5));
-    let bboxes = bboxes.into_iter().next().unwrap();
-    println!("Bboxes: {:?}", bboxes);
-    assert_eq!(bboxes.len(), 2);
+    soft_non_maximum_suppression(&mut bboxes, Some(0.5), Some(0.1), Some(0.5));
 
-    Ok(())
+    assert_eq!(bboxes[0].len(), 3);
 }
