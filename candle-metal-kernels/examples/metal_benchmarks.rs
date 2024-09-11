104615484f81c36314610f05baa97168641815a6
Laurent <laurent.mazare@gmail.com>
2024-09-11 22:47:18 +0200

    More benchmarks.

--- candle-metal-kernels/examples/metal_benchmarks.rs
+++ candle-metal-kernels/examples/metal_benchmarks.rs
@@ -1,32 +1,55 @@
 use anyhow::Result;
+use candle_metal_kernels::GemmDType;
 /// This example contains some simple benchmarks so that it's easy to run them in perf etc.
 use clap::{Parser, Subcommand};
+use half::f16;
 
-fn run_gemm(n: usize) -> Result<()> {
+fn run_gemm(f32: bool, n: usize) -> Result<()> {
     const WARMUP_ITERS: usize = 2;
     const MIN_DUR: f64 = 4.;
 
     let device = metal::Device::system_default().unwrap();
 
     let (b, m, n, k) = (1, n, n, n);
-    let lhs: Vec<f32> = (0..b * m * k).map(|f| f as f32).collect();
-    let rhs: Vec<f32> = (0..b * n * k).map(|f| f as f32).collect();
     let kernels = candle_metal_kernels::Kernels::new();
     let command_queue = device.new_command_queue();
     let options = metal::MTLResourceOptions::StorageModeManaged;
 
-    let lhs = device.new_buffer_with_data(
-        lhs.as_ptr() as *const core::ffi::c_void,
-        std::mem::size_of_val(&lhs) as u64,
-        options,
-    );
-    let rhs = device.new_buffer_with_data(
-        rhs.as_ptr() as *const core::ffi::c_void,
-        std::mem::size_of_val(&rhs) as u64,
-        options,
-    );
-    let length = b * m * n;
-    let output = device.new_buffer((length * core::mem::size_of::<f32>()) as u64, options);
+    let (lhs, rhs) = if f32 {
+        let lhs: Vec<f32> = (0..b * m * k).map(|f| f as f32).collect();
+        let rhs: Vec<f32> = (0..b * n * k).map(|f| f as f32).collect();
+        let lhs = device.new_buffer_with_data(
+            lhs.as_ptr() as *const core::ffi::c_void,
+            std::mem::size_of_val(&lhs) as u64,
+            options,
+        );
+        let rhs = device.new_buffer_with_data(
+            rhs.as_ptr() as *const core::ffi::c_void,
+            std::mem::size_of_val(&rhs) as u64,
+            options,
+        );
+        (lhs, rhs)
+    } else {
+        let lhs: Vec<f16> = (0..b * m * k).map(|f| f16::from_f32(f as f32)).collect();
+        let rhs: Vec<f16> = (0..b * n * k).map(|f| f16::from_f32(f as f32)).collect();
+        let lhs = device.new_buffer_with_data(
+            lhs.as_ptr() as *const core::ffi::c_void,
+            std::mem::size_of_val(&lhs) as u64,
+            options,
+        );
+        let rhs = device.new_buffer_with_data(
+            rhs.as_ptr() as *const core::ffi::c_void,
+            std::mem::size_of_val(&rhs) as u64,
+            options,
+        );
+        (lhs, rhs)
+    };
+    let (dtype, name, sizeof) = if f32 {
+        (GemmDType::F32, "sgemm", core::mem::size_of::<f32>())
+    } else {
+        (GemmDType::F16, "hgemm", core::mem::size_of::<f16>())
+    };
+    let output = device.new_buffer((b * m * n * sizeof) as u64, options);
 
     for mlx in [false, true] {
         let mut sum_dt = 0f64;
@@ -39,7 +62,7 @@ fn run_gemm(n: usize) -> Result<()> {
                     &device,
                     command_buffer,
                     &kernels,
-                    candle_metal_kernels::GemmDType::F32,
+                    dtype,
                     (b, m, n, k),
                     &[m * k, k, 1],
                     0,
@@ -54,8 +77,7 @@ fn run_gemm(n: usize) -> Result<()> {
                     &device,
                     command_buffer,
                     &kernels,
-                    // candle_metal_kernels::GemmDType::F32,
-                    "sgemm",
+                    name,
                     (b, m, n, k),
                     &[m * k, k, 1],
                     0,
@@ -80,7 +102,7 @@ fn run_gemm(n: usize) -> Result<()> {
         }
         let gflops = (2 * n * n * n * iters) as f64 / (1e9 * sum_dt);
         let mlx = if mlx { "MLX" } else { "MFA" };
-        println!("{mlx} f32, n {n:6}      gflops {gflops:.0}");
+        println!("{mlx} {dtype:?},      {n:6}      gflops {gflops:.0}");
     }
 
     Ok(())
@@ -103,8 +125,10 @@ fn main() -> Result<()> {
     let args = Args::parse();
     match args.task {
         Task::Gemm => {
-            for n in [512, 1024, 2048, 4096] {
-                run_gemm(n)?;
+            for f32 in [false, true] {
+                for n in [512, 1024, 2048, 4096] {
+                    run_gemm(f32, n)?;
+                }
             }
         }
     }
