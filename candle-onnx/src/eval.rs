4c33983c30c3d75c80e671aed7c479acf5dea27c
Steven Lovegrove <stevenlovegrove@gmail.com>
2024-09-25 12:16:13 -0700

    candle-onnx: Add ReduceL2 operator
    Implemented based on https://github.com/onnx/onnx/blob/main/docs/Operators.md
    Test cases based on those examples.
    
    TODO: Should add the remaining ReduceSum examples as tests

--- candle-onnx/src/eval.rs
+++ candle-onnx/src/eval.rs
@@ -1312,8 +1312,36 @@ fn simple_eval_(
 
                 values.insert(node.output[0].clone(), output);
             }
+            // https://github.com/onnx/onnx/blob/main/docs/Operators.md#ReduceL2
+            // Version 18 impl
             "ReduceL2" => {
+                let input = get(&node.input[0])?;
+                let axes = values.get(&node.input[1]);
+                let keepdims = get_attr_opt::<i64>(node, "keepdims")?.copied().unwrap_or(1);
+                let noop_with_empty_axes = get_attr_opt::<i64>(node, "noop_with_empty_axes")?.copied().unwrap_or(0);
 
+                let input_sq = input.sqr()?;
+
+                let axes = match axes {
+                    Some(axes) => {
+                        axes.to_vec1::<i64>()?.into_iter().map(|x| x as usize).collect::<Vec<_>>()
+                    }
+                    None => {
+                        if noop_with_empty_axes == 1 {
+                            vec![]
+                        }else{
+                            (0..input_sq.rank()).collect()
+                        }
+                    }
+                };
+
+                let output = if keepdims == 1 {
+                    input_sq.sum_keepdim(axes)?.sqrt()?
+                } else {
+                    input_sq.sum(axes)?.sqrt()?
+                };
+
+                values.insert(node.output[0].clone(), output);
             }
             random_type @ ("RandomUniform" | "RandomNormal") => {
                 let dt: i64 = get_attr_opt(node, "dtype")?.copied().unwrap_or(1); // 1 is float
