95a56227afa4ec1b5b3177fa97cc68103084bc70
Laurent <laurent.mazare@gmail.com>
2024-10-02 10:12:21 +0200

    Tweak some metal tests.

--- candle-metal-kernels/src/lib.rs
+++ candle-metal-kernels/src/lib.rs
@@ -2372,16 +2372,11 @@ pub fn call_const_fill(
     let pipeline = kernels.load_pipeline(device, Source::Fill, name)?;
     let encoder = ep.encoder();
     let encoder: &ComputeCommandEncoderRef = encoder.as_ref();
-
     encoder.set_compute_pipeline_state(&pipeline);
-
     set_params!(encoder, (output, v, length));
-
     let (thread_group_count, thread_group_size) = linear_split(&pipeline, length);
-
     encoder.use_resource(output, metal::MTLResourceUsage::Write);
     encoder.dispatch_thread_groups(thread_group_count, thread_group_size);
-
     Ok(())
 }
 
