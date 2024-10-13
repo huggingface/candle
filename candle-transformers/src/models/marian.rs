41ade774e8606325572215b93ef2152432997fda
Mikarific <mikerific2@gmail.com>
2024-10-13 15:05:50 -0600

    fix: Allow marian configs to deserialize from json. (#2556)

--- candle-transformers/src/models/marian.rs
+++ candle-transformers/src/models/marian.rs
@@ -1,8 +1,9 @@
 use super::with_tracing::{linear, Embedding, Linear};
 use candle::{Result, Tensor};
 use candle_nn::{layer_norm, LayerNorm, VarBuilder};
+use serde::Deserialize;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Config {
     pub vocab_size: usize,
     pub decoder_vocab_size: Option<usize>,
