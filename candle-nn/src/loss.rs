712adde369a580a351e659bfb8b8511bbc3ef15e
Benjamin Isbarn <bei@crosscan.com>
2024-10-14 11:30:40 +0200

    Fix documentation
    The documentation of binary_cross_entropy_with_logit says that it
    expects the target to be of type usize which is wrong and yields an
    error at runtime due to dtype mismatch in the multiplication step.

--- candle-nn/src/loss.rs
+++ candle-nn/src/loss.rs
@@ -55,7 +55,7 @@ pub fn mse(inp: &Tensor, target: &Tensor) -> Result<Tensor> {
 ///
 /// * [inp]: The input tensor of dimensions `N, C` where `N` is the batch size and `C` the number
 ///          of categories. This is expected to raw logits.
-/// * [target]: The ground truth labels as a tensor of u32 of dimension `N, C` where `N` is the batch size and `C` the number
+/// * [target]: The ground truth labels as a tensor of dimension `N, C` where `N` is the batch size and `C` the number
 ///          of categories.
 ///
 /// The resulting tensor is a scalar containing the average value over the batch.
