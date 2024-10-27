083d0ea650549bac1d922594b01fee517aa5da78
Laurent <laurent.mazare@gmail.com>
2024-10-27 12:44:29 +0100

    Support for UG kernels.

--- candle-core/src/lib.rs
+++ candle-core/src/lib.rs
@@ -77,7 +77,7 @@ mod variable;
 pub use cuda_backend::cudnn;
 
 pub use cpu_backend::{CpuStorage, CpuStorageRef};
-pub use custom_op::{CustomOp1, CustomOp2, CustomOp3, InplaceOp1, InplaceOp2, InplaceOp3};
+pub use custom_op::{CustomOp1, CustomOp2, CustomOp3, InplaceOp1, InplaceOp2, InplaceOp3, UgIOp1};
 pub use device::{Device, DeviceLocation, NdArray};
 pub use dtype::{DType, DTypeParseError, FloatDType, IntDType, WithDType};
 pub use error::{Error, Result};
