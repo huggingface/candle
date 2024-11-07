0638745008ef702ddeb855b77be8c78b415a46e9
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-11-07 13:42:57 +0100

    Implement StepLR

--- candle-nn/src/scheduler.rs
+++ candle-nn/src/scheduler.rs
@@ -18,16 +18,47 @@ pub struct FnLRScheduler<T> {
 
 impl<T> FnLRScheduler<T> {
     pub fn new(func: Box<dyn Fn(T) -> Result<f64>>) -> Self {
-        Self {
-            func,
-            lr: 0.0,
-        }
+        Self { func, lr: 0.0 }
     }
 }
 
 impl<T> LRScheduler<T> for FnLRScheduler<T> {
     fn step(&mut self, params: T) -> Result<f64> {
         self.lr = (self.func)(params)?;
+        Ok(self.lr)
+    }
+
+    fn get_lr(&self) -> f64 {
+        self.lr
+    }
+}
+
+/// Decays the learning rate of each parameter group by gamma every step_size epochs.
+// https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.StepLR.html#torch.optim.lr_scheduler.StepLR
+pub struct StepLR {
+    step_size: usize,
+    last_epoch: usize,
+    gamma: f64,
+    lr: f64,
+}
+
+impl StepLR {
+    pub fn new(step_size: usize, gamma: f64, lr: f64) -> Self {
+        Self {
+            step_size,
+            last_epoch: 0,
+            gamma,
+            lr,
+        }
+    }
+}
+
+impl LRScheduler<()> for StepLR {
+    fn step(&mut self, _params: ()) -> Result<f64> {
+        self.last_epoch += 1;
+        if self.last_epoch % self.step_size == 0 {
+            self.lr *= self.gamma;
+        }
         Ok(self.lr)
     }
 
