1b0ea39ab512c31deee0bdee76fe0a070c84f1d8
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-16 11:25:16 -0500

    rwkv

--- candle-transformers/src/models/rwkv_v6.rs
+++ candle-transformers/src/models/rwkv_v6.rs
@@ -1,7 +1,9 @@
 //! RWKV v6 model implementation.
 //!
-//! RWKV is an RNN with transformer-like performance.
-//! Version 6 introduces refinements to the architecture.
+//! The [RWKV model](https://wiki.rwkv.com/) is a recurrent neural network model
+//! with performance on par with transformer architectures. Several variants are
+//! available, candle implements the v5 and v6 versions and can be used with
+//! Eagle 7B([blog post](https://blog.rwkv.com/p/eagle-7b-soaring-past-transformers)).
 //!
 //! Key characteristics:
 //! - Linear attention mechanism
@@ -10,9 +12,20 @@
 //! - Feed forward gating
 //! - State recycling for efficient inference
 //!
-//! References:
-//! - [RWKV Model](https://github.com/BlinkDL/RWKV-LM)
+//! # Example
 //!
+//! ```bash
+//! cargo run --example rwkv --release -- \
+//!   --prompt "The smallest prime is "
+//!
+//! > avx: true, neon: false, simd128: false, f16c: true
+//! > temp: 0.00 repeat-penalty: 1.10 repeat-last-n: 64
+//! > The smallest prime is ϕ(2) = 2.
+//! > The smallest composite is ϕ(3) = 3.
+//! > The smallest perfect number is ϕ(5) = 5.
+//! > The smallest perfect square is ϕ(4) = 4.
+//! > The smallest perfect cube is ϕ(6) = 6.
+//! ```
 
 use super::with_tracing::{layer_norm, linear_no_bias as linear, LayerNorm, Linear};
 use candle::{IndexOp, Result, Tensor};
