339b435dc530a96aaef23cf602c7e05db7ed704b
lynxeco <kenji@lynx.eco>
2024-11-26 09:41:34 -0800

    cargo fmt --all

--- candle-examples/examples/stella-en-v5/main.rs
+++ candle-examples/examples/stella-en-v5/main.rs
@@ -6,19 +6,17 @@ extern crate accelerate_src;
 
 use std::path::Path;
 
-use anyhow::{ anyhow, Error as E, Result };
+use anyhow::{anyhow, Error as E, Result};
 use clap::Parser;
 
 use candle_transformers::models::stella_en_v5::{
-    Config,
-    EmbedDim as StellaEmbedDim,
-    EmbeddingModel,
+    Config, EmbedDim as StellaEmbedDim, EmbeddingModel,
 };
 
-use candle::{ DType, Device, Tensor };
+use candle::{DType, Device, Tensor};
 use candle_nn::VarBuilder;
-use hf_hub::{ api::sync::Api, Repo };
-use tokenizers::{ PaddingDirection, PaddingParams, PaddingStrategy, Tokenizer };
+use hf_hub::{api::sync::Api, Repo};
+use tokenizers::{PaddingDirection, PaddingParams, PaddingStrategy, Tokenizer};
 
 struct Embedding {
     model: EmbeddingModel,
@@ -61,9 +59,13 @@ impl Embedding {
 
             // We only encode the queries and not the data
             let qry = task.query_preproc(&queries);
-            let mut qry_encoded = self.tokenizer.encode_batch(qry, true).map_err(|e| anyhow!(e))?;
+            let mut qry_encoded = self
+                .tokenizer
+                .encode_batch(qry, true)
+                .map_err(|e| anyhow!(e))?;
 
-            let mut docs_encoded = self.tokenizer
+            let mut docs_encoded = self
+                .tokenizer
                 .encode_batch(docs.to_vec(), true)
                 .map_err(|e| anyhow!(e))?;
 
@@ -74,14 +76,14 @@ impl Embedding {
                 let mut masks = Tensor::zeros(shape, DType::U8, &self.device)?;
 
                 for (i, e) in qry_encoded.drain(..).enumerate() {
-                    let input_id = Tensor::from_iter(e.get_ids().to_vec(), &self.device)?.unsqueeze(
-                        0
-                    )?;
+                    let input_id =
+                        Tensor::from_iter(e.get_ids().to_vec(), &self.device)?.unsqueeze(0)?;
                     let mask = Tensor::from_iter(e.get_attention_mask().to_vec(), &self.device)?
                         .to_dtype(DType::U8)?
                         .unsqueeze(0)?;
 
-                    ids = ids.slice_assign(&[i..i + 1, 0..input_id.dims2().unwrap().1], &input_id)?;
+                    ids =
+                        ids.slice_assign(&[i..i + 1, 0..input_id.dims2().unwrap().1], &input_id)?;
                     masks = masks.slice_assign(&[i..i + 1, 0..mask.dims2().unwrap().1], &mask)?;
                 }
 
@@ -96,14 +98,14 @@ impl Embedding {
                 let mut masks = Tensor::zeros(shape, DType::U8, &self.device)?;
 
                 for (i, e) in docs_encoded.drain(..).enumerate() {
-                    let input_id = Tensor::from_iter(e.get_ids().to_vec(), &self.device)?.unsqueeze(
-                        0
-                    )?;
+                    let input_id =
+                        Tensor::from_iter(e.get_ids().to_vec(), &self.device)?.unsqueeze(0)?;
                     let mask = Tensor::from_iter(e.get_attention_mask().to_vec(), &self.device)?
                         .to_dtype(DType::U8)?
                         .unsqueeze(0)?;
 
-                    ids = ids.slice_assign(&[i..i + 1, 0..input_id.dims2().unwrap().1], &input_id)?;
+                    ids =
+                        ids.slice_assign(&[i..i + 1, 0..input_id.dims2().unwrap().1], &input_id)?;
                     masks = masks.slice_assign(&[i..i + 1, 0..mask.dims2().unwrap().1], &mask)?;
                 }
 
@@ -266,27 +268,25 @@ fn create_tokenizer(tokenizer_file: &Path, which: Which) -> Result<Tokenizer> {
         let pad_id = if let Some(pad_id) = tokenizer.token_to_id("<|endoftext|>") {
             pad_id
         } else {
-            return Err(anyhow!("Tokenizer doesn't contain expected `<|endoftext|>` token"));
+            return Err(anyhow!(
+                "Tokenizer doesn't contain expected `<|endoftext|>` token"
+            ));
         };
 
         // This part is super important, we are padding the tokens to the *`left`* and not the usual *`right`* padding
-        tokenizer.with_padding(
-            Some(PaddingParams {
-                strategy: PaddingStrategy::BatchLongest,
-                direction: PaddingDirection::Left,
-                pad_id,
-                pad_token: "<|endoftext|>".to_string(),
-                ..Default::default()
-            })
-        );
+        tokenizer.with_padding(Some(PaddingParams {
+            strategy: PaddingStrategy::BatchLongest,
+            direction: PaddingDirection::Left,
+            pad_id,
+            pad_token: "<|endoftext|>".to_string(),
+            ..Default::default()
+        }));
     } else {
-        tokenizer.with_padding(
-            Some(PaddingParams {
-                strategy: PaddingStrategy::BatchLongest,
-                direction: PaddingDirection::Right,
-                ..Default::default()
-            })
-        );
+        tokenizer.with_padding(Some(PaddingParams {
+            strategy: PaddingStrategy::BatchLongest,
+            direction: PaddingDirection::Right,
+            ..Default::default()
+        }));
     }
 
     Ok(tokenizer)
@@ -320,8 +320,14 @@ fn main() -> Result<()> {
     };
 
     let (repo, cfg) = match args.which {
-        Which::Large => ("dunzhang/stella_en_1.5B_v5", Config::new_1_5_b_v5(embed_dim.embed_dim())),
-        Which::Small => ("dunzhang/stella_en_400M_v5", Config::new_400_m_v5(embed_dim.embed_dim())),
+        Which::Large => (
+            "dunzhang/stella_en_1.5B_v5",
+            Config::new_1_5_b_v5(embed_dim.embed_dim()),
+        ),
+        Which::Small => (
+            "dunzhang/stella_en_400M_v5",
+            Config::new_400_m_v5(embed_dim.embed_dim()),
+        ),
     };
 
     let repo = api.repo(Repo::model(repo.to_string()));
@@ -333,12 +339,20 @@ fn main() -> Result<()> {
     // Note, if you are providing `weight_files`, ensure that the `--embed_dim` dimensions provided matches the weights
     // E.g. if you are using `--embed_dim 1024`, the weight files should include the `.safetensors` file from `2_Dense_1024` dir of the repo
     let base_weight_files = match args.base_weight_files {
-        Some(files) => files.split(',').map(std::path::PathBuf::from).collect::<Vec<_>>(),
-        None => { vec![repo.get("model.safetensors")?] }
+        Some(files) => files
+            .split(',')
+            .map(std::path::PathBuf::from)
+            .collect::<Vec<_>>(),
+        None => {
+            vec![repo.get("model.safetensors")?]
+        }
     };
 
     let embed_weight_files = match args.embed_head_weight_files {
-        Some(files) => files.split(',').map(std::path::PathBuf::from).collect::<Vec<_>>(),
+        Some(files) => files
+            .split(',')
+            .map(std::path::PathBuf::from)
+            .collect::<Vec<_>>(),
         None => {
             let head_w_path = format!("{}/model.safetensors", embed_dim.embed_dim_default_dir());
             vec![repo.get(&head_w_path)?]
@@ -355,13 +369,11 @@ fn main() -> Result<()> {
     let device = candle_examples::device(args.cpu)?;
     let dtype = DType::F32;
 
-    let base_vb = unsafe {
-        VarBuilder::from_mmaped_safetensors(&base_weight_files, dtype, &device)?
-    };
+    let base_vb =
+        unsafe { VarBuilder::from_mmaped_safetensors(&base_weight_files, dtype, &device)? };
     // Embedding layer is always built on F32 for accuracy
-    let embed_vb = unsafe {
-        VarBuilder::from_mmaped_safetensors(&embed_weight_files, DType::F32, &device)?
-    };
+    let embed_vb =
+        unsafe { VarBuilder::from_mmaped_safetensors(&embed_weight_files, DType::F32, &device)? };
 
     let model = EmbeddingModel::new(&cfg, base_vb, embed_vb)?;
 
