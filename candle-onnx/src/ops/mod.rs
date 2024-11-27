4dbdb9d44ce5a50e1f3a736513f211cde0e2c872
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-11-27 20:36:51 +0100

    add sign operation and setup the registry

--- candle-onnx/src/ops/mod.rs
+++ candle-onnx/src/ops/mod.rs
@@ -3,3 +3,13 @@ pub use onnxop::{OnnxOp, OnnxOpError, OnnxOpRegistry, OpOutput};
 
 pub mod compute_node;
 pub use compute_node::ComputeNode;
+
+
+mod math;
+use math::sign;
+
+pub fn registry() -> Result<OnnxOpRegistry, OnnxOpError> {
+    let mut registry = OnnxOpRegistry::new();
+    registry.insert("Sign", Box::new(sign::Sign))?;
+    Ok(registry)
+}
