dc0d2d50dca1b4d09dab8896bb99d4e10ccb53c8
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-11-27 20:45:03 +0100

    cargo fmt

--- candle-onnx/src/ops/math/sign.rs
+++ candle-onnx/src/ops/math/sign.rs
@@ -1,17 +1,20 @@
 use crate::ops::compute_node::ComputeNode;
-use crate::ops::{OnnxOp, OnnxOpError, OpOutput};
 use crate::ops::OnnxOpError::ComputationFailed;
+use crate::ops::{OnnxOp, OnnxOpError, OpOutput};
 
 pub(crate) struct Sign;
 impl OnnxOp for Sign {
     fn eval(&self, node: &ComputeNode) -> Result<OpOutput, OnnxOpError> {
-        let input = node.get_input(0)
+        let input = node
+            .get_input(0)
             .ok_or_else(|| ComputationFailed("input 0 not found".to_string()))?;
 
-        let output = input.sign()
-            .map_err(|err| ComputationFailed(format!("{:?}",err)))?;
+        let output = input
+            .sign()
+            .map_err(|err| ComputationFailed(format!("{:?}", err)))?;
 
-        let output_name = node.get_output(0)
+        let output_name = node
+            .get_output(0)
             .ok_or_else(|| ComputationFailed("output 0 not found".to_string()))?;
 
         Ok((output_name.clone(), output))
--- candle-onnx/src/ops/mod.rs
+++ candle-onnx/src/ops/mod.rs
@@ -4,7 +4,6 @@ pub use onnxop::{OnnxOp, OnnxOpError, OnnxOpRegistry, OpOutput};
 pub mod compute_node;
 pub use compute_node::ComputeNode;
 
-
 mod math;
 use math::sign;
 
