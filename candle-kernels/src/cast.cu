a3814f5a3a040ef679984b2e913b2f98911b9e48
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2024-12-01 21:25:35 -0500

    Fix duplicate cuda cast instantiations (#51)

--- candle-kernels/src/cast.cu
+++ candle-kernels/src/cast.cu
@@ -174,20 +174,6 @@ CAST_OP_FP8_INTO(int32_t,   __nv_fp8_e4m3, cast_i32_f8_e4m3)
 CAST_OP_FP8(__nv_fp8_e4m3, int32_t, cast_f8_e4m3_i32)
 CAST_OP_FP8(__nv_fp8_e4m3, __nv_bfloat16, cast_f8_e4m3_bf16)
 CAST_OP_FP8_INTO(__nv_bfloat16, __nv_fp8_e4m3, cast_bf16_f8_e4m3)
-#include <cuda.h>
-#if CUDA_VERSION >= 11000
-CAST_OP(__nv_bfloat16, float,    cast_bf16_f32)
-CAST_OP(float,    __nv_bfloat16, cast_f32_bf16)
-CAST_THROUGH_OP(__nv_bfloat16, uint8_t, float, cast_bf16_u8)
-CAST_THROUGH_OP(__nv_bfloat16, __half,  float, cast_bf16_f16)
-CAST_THROUGH_OP(__nv_bfloat16, double,  float, cast_bf16_f64)
-CAST_THROUGH_OP(__half,   __nv_bfloat16, float, cast_f16_bf16)
-CAST_THROUGH_OP(double,   __nv_bfloat16, float, cast_f64_bf16)
-CAST_THROUGH_OP(uint8_t,   __nv_bfloat16, float, cast_u8_bf16)
-CAST_THROUGH_OP(int32_t,   __nv_bfloat16, float, cast_i32_bf16)
-CAST_THROUGH_OP(__nv_bfloat16, int32_t, float, cast_bf16_i32)
-CAST_THROUGH_OP(__nv_bfloat16, __nv_fp8_e4m3, float, cast_bf16_f8_e4m3)
-#endif
 #endif
 
 #if __CUDA_ARCH__ >= 530
