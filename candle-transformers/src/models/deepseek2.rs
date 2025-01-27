a1dd5a125c681c126f9927ef65726b634be01964
Eric Buehler <ericlbuehler@gmail.com>
2025-01-27 13:08:21 -0500

    Remove unused

--- candle-transformers/src/models/deepseek2.rs
+++ candle-transformers/src/models/deepseek2.rs
@@ -33,50 +33,6 @@ impl NonZero {
     }
 }
 
-#[cfg(feature = "cuda")]
-fn count_nonzero_cuda(dtype: candle::DType, d_in: *const c_void, n: u32) -> u32 {
-    unsafe {
-        match dtype {
-            candle::DType::U8 => ffi::count_nonzero_u8(d_in, n),
-            candle::DType::U32 => ffi::count_nonzero_u32(d_in, n),
-            candle::DType::I64 => ffi::count_nonzero_i64(d_in, n),
-            candle::DType::I16 => ffi::count_nonzero_i16(d_in, n),
-            candle::DType::I32 => ffi::count_nonzero_i32(d_in, n),
-            candle::DType::BF16 => ffi::count_nonzero_bf16(d_in, n),
-            candle::DType::F16 => ffi::count_nonzero_f16(d_in, n),
-            candle::DType::F32 => ffi::count_nonzero_f32(d_in, n),
-            candle::DType::F64 => ffi::count_nonzero_f64(d_in, n),
-            candle::DType::F8E4M3 => todo!(),
-        }
-    }
-}
-
-#[cfg(feature = "cuda")]
-fn nonzero_cuda(
-    dtype: candle::DType,
-    d_in: *const c_void,
-    n: u32,
-    num_nonzero: u32,
-    dims: *const c_void,
-    num_dims: u32,
-    d_out: *mut c_void,
-) {
-    unsafe {
-        match dtype {
-            candle::DType::U8 => ffi::nonzero_u8(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::U32 => ffi::nonzero_u32(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::I64 => ffi::nonzero_i64(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::I32 => ffi::nonzero_i64(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::I16 => ffi::nonzero_i16(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::BF16 => ffi::nonzero_bf16(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::F16 => ffi::nonzero_f16(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::F32 => ffi::nonzero_f32(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::F64 => ffi::nonzero_f64(d_in, n, num_nonzero, dims, num_dims, d_out),
-            candle::DType::F8E4M3 => todo!(),
-        }
-    }
-}
-
 impl CustomOp1 for NonZero {
     fn name(&self) -> &'static str {
         "nonzero"
