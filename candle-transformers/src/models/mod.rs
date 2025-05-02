8a4f4b463b654ddc8724e7729afda524664d36c7
keighbee <kb@huggingface.co>
2025-05-02 15:49:22 -0700

    fixed rebase error

--- candle-transformers/src/models/mod.rs
+++ candle-transformers/src/models/mod.rs
@@ -79,6 +79,7 @@ pub mod phi3;
 pub mod pixtral;
 pub mod quantized_blip;
 pub mod quantized_blip_text;
+pub mod quantized_gemma3;
 pub mod quantized_llama;
 pub mod quantized_llama2_c;
 pub mod quantized_metavoice;
