37e8e7560e1fc35b79e73f41e70925c620771ba7
laurent <laurent.mazare@gmail.com>
2025-05-03 11:34:05 +0200

    Fix.

--- candle-core/tests/tensor_tests.rs
+++ candle-core/tests/tensor_tests.rs
@@ -845,7 +845,6 @@ fn embeddings(device: &Device) -> Result<()> {
     assert_eq!(hs.to_vec2::<f32>()?, &[[0.0, 1.0], [4.0, 5.0], [2.0, 3.0]]);
     let hs = t.index_select(&ids.to_dtype(DType::I64)?, 0)?;
     assert_eq!(hs.to_vec2::<f32>()?, &[[0.0, 1.0], [4.0, 5.0], [2.0, 3.0]]);
-    let hs = t.index_select(&ids, 0)?;
     let ids = Tensor::new(&[u32::MAX, 2u32, u32::MAX], device)?;
     let hs = t.index_select(&ids, 0)?;
     assert_eq!(hs.to_vec2::<f32>()?, &[[0.0, 0.0], [4.0, 5.0], [0.0, 0.0]]);
