ee32338a91ff113b09e4ab0917fc39a13bcd832c
misadowsk <michalsad.protondynamic@gmail.com>
2025-05-25 12:38:04 +0200

    deleted elu

--- candle-onnx/src/eval.rs
+++ candle-onnx/src/eval.rs
@@ -986,11 +986,6 @@ fn simple_eval_(
                 let output = input.gelu_erf()?;
                 values.insert(node.output[0].clone(), output);
             }
-            "Elu" => {
-                let input = get(&node.input[0])?;
-                let output = input.gelu_erf()?;
-                values.insert(node.output[0].clone(), output);
-            }
             "Relu" => {
                 let input = get(&node.input[0])?;
                 let output = input.relu()?;
