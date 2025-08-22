b2660c46d4f89bec6e4803b407f5d053456b3f4a
Bai Li <bai.li.2005@gmail.com>
2025-08-22 16:12:24 -0700

    able to build now

--- candle-wasm-examples/phi/build-lib.sh
+++ candle-wasm-examples/phi/build-lib.sh
@@ -1,5 +1,4 @@
 set -e
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen ../../target/wasm32-unknown-unknown/release/m.wasm --out-dir build --target web --split-linked-modules
-wasm-opt -Oz --enable-threads --enable-bulk-memory build/m_bg.wasm -o build/m_bg.wasm
 python server.py
