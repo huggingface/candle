aa0d99940b1999b7d12e0688b202a97f946667e2
Bai Li <bai.li.2005@gmail.com>
2025-08-22 16:59:04 -0700

    fix cors headers

--- candle-wasm-examples/phi/server.py
+++ candle-wasm-examples/phi/server.py
@@ -6,7 +6,7 @@ from http.server import SimpleHTTPRequestHandler
 class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header("Cross-Origin-Opener-Policy", "same-origin")
-        self.send_header("Cross-Origin-Embedder-Policy", "same-origin")
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         super().end_headers()
 
 
