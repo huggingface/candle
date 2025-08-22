c1b216300879bffc3a592adb5fba2caace870196
Bai Li <bai.li.2005@gmail.com>
2025-08-22 15:59:51 -0700

    add rust-toolchain.toml

--- candle-wasm-examples/phi/build-lib.sh
+++ candle-wasm-examples/phi/build-lib.sh
@@ -1,5 +1,4 @@
 set -e
-export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals"
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen ../../target/wasm32-unknown-unknown/release/m.wasm --out-dir build --target web --split-linked-modules
 wasm-opt -Oz --enable-threads --enable-bulk-memory build/m_bg.wasm -o build/m_bg.wasm
