5743983c5f5c9a647cde21573e4b056ddeb221a0
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2025-08-26 10:58:41 +0200

    Refactor metal blit and compute command encoders

--- candle-metal-kernels/src/metal/mod.rs
+++ candle-metal-kernels/src/metal/mod.rs
@@ -1,11 +1,13 @@
 pub mod buffer;
 pub mod command_buffer;
 pub mod device;
+pub mod encoder;
 pub mod library;
 pub mod metal_utils;
 
 pub use buffer::*;
 pub use command_buffer::*;
 pub use device::*;
+pub use encoder::*;
 pub use library::*;
 pub use metal_utils::*;
