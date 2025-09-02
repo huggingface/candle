c2f2dd4fbae24ae049f6930c5a5e44afc5ebe04f
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2025-09-02 22:04:11 +0200

    Update examples

--- candle-core/examples/basics.rs
+++ candle-core/examples/basics.rs
@@ -5,11 +5,11 @@ extern crate intel_mkl_src;
 extern crate accelerate_src;
 
 use anyhow::Result;
-use candle_core::{Device, Tensor};
+use candle_core::{cpu_backend::CpuDevice, CpuStorage, Tensor};
 
 fn main() -> Result<()> {
-    let a = Tensor::new(&[[0.0f32, 1.0, 2.0], [3.0, 4.0, 5.0]], &Device::Cpu)?;
-    let b = Tensor::new(&[[88.0f32, 99.0]], &Device::Cpu)?;
+    let a: Tensor<CpuStorage> = Tensor::new(&[[0.0f32, 1.0, 2.0], [3.0, 4.0, 5.0]], &CpuDevice)?;
+    let b = Tensor::new(&[[88.0f32, 99.0]], &CpuDevice)?;
     let new_a = a.slice_scatter(&b, 1, 2)?;
     assert_eq!(a.to_vec2::<f32>()?, [[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]]);
     assert_eq!(new_a.to_vec2::<f32>()?, [[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]]);
--- candle-core/examples/cuda_sum_benchmark.rs
+++ candle-core/examples/cuda_sum_benchmark.rs
@@ -7,9 +7,14 @@ extern crate accelerate_src;
 use std::str::FromStr;
 
 use anyhow::Result;
-use candle_core::{Device, Tensor};
+use candle_core::{
+    backend::BackendDevice, cpu_backend::CpuDevice, BackendStorage, CpuStorage, CudaDevice,
+    CudaStorage, Tensor,
+};
 
-fn cos_sin(n: usize, device: &Device) -> Result<Tensor> {
+type CudaTensor = Tensor<CudaStorage>;
+
+fn cos_sin<B: BackendStorage>(n: usize, device: &B::Device) -> Result<Tensor<B>> {
     let thetas: Vec<_> = (0..n).map(|i| (i as f32 / n as f32)).collect();
     let xs: Vec<_> = thetas.iter().map(|t| t.cos().abs()).collect();
     let ys: Vec<_> = thetas.iter().map(|t| t.sin().abs()).collect();
@@ -20,15 +25,15 @@ fn cos_sin(n: usize, device: &Device) -> Result<Tensor> {
 }
 
 fn main() -> Result<()> {
-    let device = Device::new_cuda(0)?;
+    let device = CudaDevice::new(0)?;
     let args = std::env::args().collect::<Vec<String>>();
     let n = if args.len() < 2 {
         2000usize
     } else {
         usize::from_str(&args[1])?
     };
-    let xys_cpu = cos_sin(n, &Device::Cpu)?;
-    let xys = cos_sin(n, &device)?;
+    let xys_cpu: Tensor<CpuStorage> = cos_sin(n, &CpuDevice)?;
+    let xys: CudaTensor = cos_sin(n, &device)?;
     println!("{xys_cpu:?} {xys:?}");
     let sum_keepdim_cpu = xys_cpu.sum_keepdim(1)?;
     println!("{sum_keepdim_cpu}");
