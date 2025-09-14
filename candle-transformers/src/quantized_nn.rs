d7060c69e9acc5bac9928e00aa50cf87d40c27f8
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2025-09-14 17:12:12 +0200

    update candle-transformers quantized_nn

--- candle-transformers/src/quantized_nn.rs
+++ candle-transformers/src/quantized_nn.rs
@@ -7,53 +7,59 @@
 use crate::models::with_tracing::QMatMul;
 use crate::quantized_var_builder::VarBuilder;
 use candle::quantized::{QTensor, QuantizedBackend};
-use candle::{BackendStorage, Module, Result, Tensor};
+use candle::{Module, Result, Tensor};
 
 #[derive(Debug, Clone)]
-pub struct Embedding<B: BackendStorage> {
-    inner: candle_nn::Embedding<B>,
+pub struct Embedding<QB: QuantizedBackend> {
+    inner: candle_nn::Embedding<QB::Storage>,
     span: tracing::Span,
 }
 
-impl<B: BackendStorage> Embedding<B> {
-    pub fn new(d1: usize, d2: usize, vb: VarBuilder<B>) -> Result<Self> {
+impl<QB: QuantizedBackend> Embedding<QB> {
+    pub fn new(d1: usize, d2: usize, vb: VarBuilder<QB>) -> Result<Self> {
         let embeddings = vb.get((d1, d2), "weight")?.dequantize(vb.device())?;
         let inner = candle_nn::Embedding::new(embeddings, d2);
         let span = tracing::span!(tracing::Level::TRACE, "embedding");
         Ok(Self { inner, span })
     }
 
-    pub fn embeddings(&self) -> &Tensor<B> {
+    pub fn embeddings(&self) -> &Tensor<QB::Storage> {
         self.inner.embeddings()
     }
 }
 
-impl<B: BackendStorage> Module<B> for Embedding<B> {
-    fn forward(&self, xs: &Tensor<B>) -> Result<Tensor<B>> {
+impl<QB: QuantizedBackend> Module<QB::Storage> for Embedding<QB> {
+    fn forward(&self, xs: &Tensor<QB::Storage>) -> Result<Tensor<QB::Storage>> {
         let _enter = self.span.enter();
         self.inner.forward(xs)
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct Linear<B: BackendStorage, QB: QuantizedBackend> {
-    weight: QMatMul<B, QB>,
-    bias: Option<Tensor<B>>,
+pub struct Linear<QB: QuantizedBackend> {
+    weight: QMatMul<QB>,
+    bias: Option<Tensor<QB::Storage>>,
 }
 
-impl<B: BackendStorage, QB: QuantizedBackend> Linear<B, QB> {
-    pub fn from_arc(weight: std::sync::Arc<QTensor>, bias: Option<Tensor>) -> Result<Self> {
+impl<QB: QuantizedBackend> Linear<QB> {
+    pub fn from_arc(
+        weight: std::sync::Arc<QTensor<QB>>,
+        bias: Option<Tensor<QB::Storage>>,
+    ) -> Result<Self> {
         let weight = QMatMul::from_weights(weight)?;
         Ok(Self { weight, bias })
     }
 
-    pub fn from_weights(weight: QMatMul, bias: Option<Tensor>) -> Self {
+    pub fn from_weights(weight: QMatMul<QB>, bias: Option<Tensor<QB::Storage>>) -> Self {
         Self { weight, bias }
     }
 }
 
-impl Module for Linear {
-    fn forward(&self, x: &Tensor) -> candle::Result<Tensor> {
+impl<QB: QuantizedBackend> Module<QB::Storage> for Linear<QB>
+where
+    QMatMul<QB>: Module<QB::Storage>,
+{
+    fn forward(&self, x: &Tensor<QB::Storage>) -> candle::Result<Tensor<QB::Storage>> {
         let x = x.apply(&self.weight)?;
         match &self.bias {
             None => Ok(x),
@@ -62,7 +68,12 @@ impl Module for Linear {
     }
 }
 
-pub fn linear_b(in_dim: usize, out_dim: usize, bias: bool, vb: VarBuilder) -> Result<Linear> {
+pub fn linear_b<QB: QuantizedBackend>(
+    in_dim: usize,
+    out_dim: usize,
+    bias: bool,
+    vb: VarBuilder<QB>,
+) -> Result<Linear<QB>> {
     let bias = if bias {
         Some(vb.get(out_dim, "bias")?.dequantize(vb.device())?)
     } else {
@@ -72,7 +83,11 @@ pub fn linear_b(in_dim: usize, out_dim: usize, bias: bool, vb: VarBuilder) -> Re
     Ok(Linear { weight, bias })
 }
 
-pub fn linear(in_dim: usize, out_dim: usize, vb: VarBuilder) -> Result<Linear> {
+pub fn linear<QB: QuantizedBackend>(
+    in_dim: usize,
+    out_dim: usize,
+    vb: VarBuilder<QB>,
+) -> Result<Linear<QB>> {
     let bias = vb.get(out_dim, "bias")?.dequantize(vb.device())?;
     let weight = QMatMul::new(in_dim, out_dim, vb)?;
     Ok(Linear {
@@ -81,45 +96,57 @@ pub fn linear(in_dim: usize, out_dim: usize, vb: VarBuilder) -> Result<Linear> {
     })
 }
 
-pub fn layer_norm(size: usize, eps: f64, vb: VarBuilder) -> Result<candle_nn::LayerNorm> {
+pub fn layer_norm<QB: QuantizedBackend>(
+    size: usize,
+    eps: f64,
+    vb: VarBuilder<QB>,
+) -> Result<candle_nn::LayerNorm<QB::Storage>> {
     let weight = vb.get(size, "weight")?.dequantize(vb.device())?;
     let bias = vb.get(size, "bias")?.dequantize(vb.device())?;
     Ok(candle_nn::LayerNorm::new(weight, bias, eps))
 }
 
-pub fn layer_norm_no_bias(size: usize, eps: f64, vb: VarBuilder) -> Result<candle_nn::LayerNorm> {
+pub fn layer_norm_no_bias<QB: QuantizedBackend>(
+    size: usize,
+    eps: f64,
+    vb: VarBuilder<QB>,
+) -> Result<candle_nn::LayerNorm<QB::Storage>> {
     let weight = vb.get(size, "weight")?.dequantize(vb.device())?;
     Ok(candle_nn::LayerNorm::new_no_bias(weight, eps))
 }
 
-pub fn linear_no_bias(in_dim: usize, out_dim: usize, vb: VarBuilder) -> Result<Linear> {
+pub fn linear_no_bias<QB: QuantizedBackend>(
+    in_dim: usize,
+    out_dim: usize,
+    vb: VarBuilder<QB>,
+) -> Result<Linear<QB>> {
     let weight = QMatMul::new(in_dim, out_dim, vb)?;
     Ok(Linear { weight, bias: None })
 }
 
 #[derive(Debug, Clone)]
-pub struct RmsNorm {
-    weight: Tensor,
+pub struct RmsNorm<QB: QuantizedBackend> {
+    weight: Tensor<QB::Storage>,
     eps: f64,
     span: tracing::Span,
 }
 
-impl RmsNorm {
-    pub fn new(size: usize, eps: f64, vb: VarBuilder) -> Result<Self> {
+impl<QB: QuantizedBackend> RmsNorm<QB> {
+    pub fn new(size: usize, eps: f64, vb: VarBuilder<QB>) -> Result<Self> {
         let span = tracing::span!(tracing::Level::TRACE, "rms-norm");
         let weight = vb.get(size, "weight")?.dequantize(vb.device())?;
         Ok(Self { weight, eps, span })
     }
 
-    pub fn from_qtensor(weight: QTensor, eps: f64) -> Result<Self> {
+    pub fn from_qtensor(weight: QTensor<QB>, eps: f64) -> Result<Self> {
         let span = tracing::span!(tracing::Level::TRACE, "rms-norm");
         let weight = weight.dequantize(&weight.device())?;
         Ok(Self { weight, eps, span })
     }
 }
 
-impl Module for RmsNorm {
-    fn forward(&self, x: &Tensor) -> Result<Tensor> {
+impl<QB: QuantizedBackend> Module<QB::Storage> for RmsNorm<QB> {
+    fn forward(&self, x: &Tensor<QB::Storage>) -> Result<Tensor<QB::Storage>> {
         let _enter = self.span.enter();
         candle_nn::ops::rms_norm(x, &self.weight, self.eps as f32)
     }
