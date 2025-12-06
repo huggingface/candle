1179f2fa0f161d797da7cedffcf5b915c608b030
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2025-12-06 18:05:50 +0100

    Restore benchmarks

--- candle-core/benches/bench_main.rs
+++ candle-core/benches/bench_main.rs
@@ -3,14 +3,14 @@ mod benchmarks;
 use criterion::criterion_main;
 
 criterion_main!(
-    //benchmarks::affine::benches,
-    //benchmarks::broadcast::benches,
-    //benchmarks::copy::benches,
-    //benchmarks::conv_transpose2d::benches,
-    //benchmarks::matmul::benches,
-    //benchmarks::qmatmul::benches,
-    //benchmarks::random::benches,
-    //benchmarks::reduce::benches,
+    benchmarks::affine::benches,
+    benchmarks::broadcast::benches,
+    benchmarks::copy::benches,
+    benchmarks::conv_transpose2d::benches,
+    benchmarks::matmul::benches,
+    benchmarks::qmatmul::benches,
+    benchmarks::random::benches,
+    benchmarks::reduce::benches,
     benchmarks::unary::benches,
-    //benchmarks::where_cond::benches,
+    benchmarks::where_cond::benches,
 );
