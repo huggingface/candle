5b5c47b1353f0cd83a84f10c939e48e5254f98b3
ivarflakstad <69173633+ivarflakstad@users.noreply.github.com>
2026-01-14 15:00:04 +0100

    Update candle-nn/benches/bench_main.rs

--- candle-nn/benches/bench_main.rs
+++ candle-nn/benches/bench_main.rs
@@ -4,7 +4,6 @@ use criterion::criterion_main;
 criterion_main!(
     benchmarks::norm::benches,
     benchmarks::softmax::benches,
-    benchmarks::layer_norm::benches,
     benchmarks::conv::benches,
     benchmarks::cpu_flash_attention::benches,
     benchmarks::varlen_attention::benches
