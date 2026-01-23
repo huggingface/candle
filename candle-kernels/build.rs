39e7bc26a773774022eb98f53008e405977b2d27
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2026-01-23 15:49:14 +0100

    Use upstream bindgen_cuda crate. Use separate builders for each steps. Remove some cargo build warning messages

--- candle-kernels/build.rs
+++ candle-kernels/build.rs
@@ -10,16 +10,20 @@ fn main() {
     // Build for PTX
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let ptx_path = out_dir.join("ptx.rs");
-    let mut builder = bindgen_cuda::Builder::default()
+    let builder = bindgen_cuda::Builder::default()
         .arg("--expt-relaxed-constexpr")
         .arg("-std=c++17")
         .arg("-O3");
-    println!("cargo::warning={builder:?}");
     let bindings = builder.build_ptx().unwrap();
     bindings.write(&ptx_path).unwrap();
 
     // Remove unwanted MOE PTX constants from ptx.rs
     remove_lines(&ptx_path, &["MOE_GGUF", "MOE_WMMA", "MOE_WMMA_GGUF"]);
+
+    let moe_builder = bindgen_cuda::Builder::default()
+        .arg("--expt-relaxed-constexpr")
+        .arg("-std=c++17")
+        .arg("-O3");
 
     // Build for FFI binding (must use custom bindgen_cuda, which supports simutanously build PTX and lib)
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -27,21 +31,21 @@ fn main() {
     if let Ok(target) = std::env::var("TARGET") {
         if target.contains("msvc") {
             is_target_msvc = true;
-            builder = builder.arg("-D_USE_MATH_DEFINES");
+            moe_builder = moe_builder.arg("-D_USE_MATH_DEFINES");
         }
     }
 
     if !is_target_msvc {
-        builder = builder.arg("-Xcompiler").arg("-fPIC");
+        moe_builder = moe_builder.arg("-Xcompiler").arg("-fPIC");
     }
 
-    let builder = builder.kernel_paths(vec![
+    let moe_builder = moe_builder.kernel_paths(vec![
         "src/moe/moe_gguf.cu",
         "src/moe/moe_wmma.cu",
         "src/moe/moe_wmma_gguf.cu",
     ]);
     println!("cargo::warning={builder:?}");
-    builder.build_lib(out_dir.join("libmoe.a"));
+    moe_builder.build_lib(out_dir.join("libmoe.a"));
     println!("cargo:rustc-link-search={}", out_dir.display());
     println!("cargo:rustc-link-lib=moe");
     println!("cargo:rustc-link-lib=dylib=cudart");
