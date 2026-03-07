503e0115020dfa2265968e5a7b1ba51d4da921fa
Andris Vāravs <andris.varavs@lmt.lv>
2026-03-07 14:40:24 +0200

    cargo fmt

--- candle-core/src/conv.rs
+++ candle-core/src/conv.rs
@@ -305,15 +305,13 @@ impl Tensor {
                 bias.layout(),
                 params,
             )?;
-            let op = BackpropOp::new3(self, kernel, bias, |arg, kernel, bias| {
-                Op::Conv2DBias {
-                    arg,
-                    kernel,
-                    bias,
-                    padding: params.padding,
-                    stride: params.stride,
-                    dilation: params.dilation,
-                }
+            let op = BackpropOp::new3(self, kernel, bias, |arg, kernel, bias| Op::Conv2DBias {
+                arg,
+                kernel,
+                bias,
+                padding: params.padding,
+                stride: params.stride,
+                dilation: params.dilation,
             });
             let out_dims = params.out_dims();
             Ok(crate::tensor::from_storage(storage, out_dims, op, false))
