9bcfd982ee6500d831706fe0c351ce627c0d58fb
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2026-06-25 14:53:54 +0200

    Add paged flash-attn kernels (#3655)

--- candle-flash-attn/kernels/flash_fwd_splitkv_hdim512_fp16_causal_sm80.cu
+++ candle-flash-attn/kernels/flash_fwd_splitkv_hdim512_fp16_causal_sm80.cu
@@ -1,6 +1,5 @@
 // Copyright (c) 2024, Tri Dao.
 // Splitting the different head dimensions to different files to speed up compilation.
-// This file is auto-generated. See "generate_kernels.py"
 
 // hd512 (Gemma 4) uses no alibi/softcap/local; disabling shrinks the template cross-product so ptxas does not take hours
 #define FLASHATTENTION_DISABLE_ALIBI
@@ -10,6 +9,6 @@
 #include "flash_fwd_launch_template.h"
 
 template<>
-void run_mha_fwd_<cutlass::bfloat16_t, 512, true>(Flash_fwd_params &params, cudaStream_t stream) {
-    run_mha_fwd_hdim512<cutlass::bfloat16_t, true>(params, stream);
+void run_mha_fwd_splitkv_paged_<cutlass::half_t, 512, true>(Flash_fwd_params &params, cudaStream_t stream) {
+    run_mha_fwd_splitkv_paged_hdim512<cutlass::half_t, true>(params, stream);
 }
