f83be86d8dec8514c69e686ae2e1435652c210ec
Andreas Schneider <asn@cryptomilk.org>
2026-07-30 11:53:23 +0200

    candle-onnx: Fix test_gelu_operation test
    thread 'test_gelu_operation' (1632813) panicked at tests/ops.rs:1799:5:
    assertion `left == right` failed
      left: [[0.0, 0.8413447], [1.9544997, 2.9959502]]
     right: [[0.0, 0.8413448], [1.9544997, 2.9959502]]
    
    The to_vec2_round() is already used in different tests in this file.

--- candle-onnx/tests/ops.rs
+++ candle-onnx/tests/ops.rs
@@ -1794,11 +1794,9 @@ fn test_gelu_operation() -> Result<()> {
 
     let z = eval.get(OUTPUT_Z).expect("Output 'z' not found");
 
-    let results = z.to_vec2::<f32>()?;
-
     assert_eq!(
-        results,
-        vec![vec![0.0, 0.8413448], vec![1.9544997, 2.9959502]]
+        to_vec2_round(z, 4)?,
+        vec![vec![0.0, 0.8413], vec![1.9545, 2.996]]
     );
 
     Ok(())
